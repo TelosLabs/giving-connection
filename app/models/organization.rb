8106c51240b5c0c5c7ca8a540a54067e152e1fe7
evertsalinas <evertsalinas88@gmail.com>
2021-10-01 13:06:34 -0500

    remove extra space

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ApplicationRecord
   include OrganizationConstants
 
   belongs_to :creator , polymorphic: true
-  
+
   validates :name, presence: true, uniqueness: true
   validates :ein_number, presence: true, uniqueness: true
   validates :irs_ntee_code, presence: true, inclusion: { in: OrganizationConstants::NTEE_CODE }
@@ -11,6 +11,4 @@ class Organization < ApplicationRecord
   validates :tagline_en, presence: true
   validates :description_en, presence: true
   validates :scope_of_work, presence: true, inclusion: { in: OrganizationConstants::SCOPE }
-
 end
-
