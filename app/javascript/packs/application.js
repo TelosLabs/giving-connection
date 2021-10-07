4f127eacd8308208795029fc8a6d55e7ba70b367
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-10-06 23:05:23 -0600

    fix error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import '../stylesheets/application'
 import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,4 +13,3 @@ Rails.start()
 ActiveStorage.start()
 
 import "controllers"
-import "../stylesheets/application.scss"
