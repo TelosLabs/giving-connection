59e3d198956d7e54f8d964f94e35d518668046e1
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-10-13 20:36:51 -0600

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        blue: {
+          dark: '#113c7b',
+          medium: '#0782d0',
+          light: '#9abee3',
+          pale: '#e2ecf7'
+        },
+        seafoam: {
+          DEFAULT: '#9ae2e0'
+        },
+        salmon: {
+          DEFAULT: '#fc8383'
+        },
+        gray: {
+          'gray-1': '#f9fafc',
+          'gray-2': '#eff2f7',
+          'gray-3': '#e5e9f2',
+          'gray-4': '#D3dce6',
+          'gray-5': '#c2cedB',
+          'gray-6': '#8492a6',
+          'gray-7': '#3c4858'
+        },
+        states: {
+          success: '#37ccb2',
+          error: '#f95b6e',
+          warning: '#febb5a',
+          disabled: '#eff2f7'
+        },
+        copy: {
+          white: '#ffffff'
+        }
+      },
+    },
   },
   variants: {
     extend: {},
