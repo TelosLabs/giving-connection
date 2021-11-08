7a94eb100702686f2788c63bb8981e3f2635ad09
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-08 09:47:47 -0600

    add sidebar design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          'gray-3': '#3c4858',
+          'gray-4': '#8492a6',
+          'gray-5': '#c2cedB',
+          'gray-6': '#D3dce6',
+          'gray-7': '#e5e9f2',
+          'gray-8': '#eff2f7',
+          'gray-9': '#f9fafc'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
