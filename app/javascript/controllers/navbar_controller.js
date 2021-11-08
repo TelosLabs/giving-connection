a9761b64da24b3cf1f7017fee5039d70d8b93bc6
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-08 10:49:10 -0600

    add sideBar controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static get targets() {
-    return ['sideNavbar']
+    return [ "sideNavBar" ]
   }
 
+  openSideNavBar() {
+    this.sideNavBarTarget.classList.toggle('hidden')
+  }
+
+  collapseSideNavBar() {
+    this.sideNavBarTarget.classList.add('hidden')
+  }
 }
