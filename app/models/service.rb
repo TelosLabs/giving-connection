407323c7d06659c9df9ac4cb1801331401e1bf5e
evertsalinas <evertsalinas88@gmail.com>
2021-11-08 19:02:53 -0600

    Annotate update

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: services
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  location_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Service < ApplicationRecord
   belongs_to :location, optional: true
   validates :name, presence: true
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -11,9 +11,14 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-FactoryBot.define do
-  factory :tag do
-    name { 'Special Care' }
-    organization { association :organization }
+require 'rails_helper'
+
+RSpec.describe Service, type: :model do
+  context 'Service model validation test' do
+    subject { create(:service) }
+
+    it 'ensures service can be created' do
+      expect(subject).to be_valid
+    end
   end
 end
