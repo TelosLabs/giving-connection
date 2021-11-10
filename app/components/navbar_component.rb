264269dd6303fc6abd636bc57181fa6d09fbc6d1
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-09 18:56:29 -0600

    rubocop style/doc

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# navbar view component
 class NavbarComponent < ViewComponent::Base
   def initialize(signed_in:)
     @signed_in = signed_in
--- app/components/side_navbar_component.rb
+++ app/components/side_navbar_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# sidebar view component
 class SideNavbarComponent < ViewComponent::Base
   def initialize(signed_in:)
     @signed_in = signed_in
