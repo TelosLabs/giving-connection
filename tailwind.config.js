7c95039268d24b59e88ad65502082244812c7f99
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-16 17:40:10 -0600

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,13 @@ module.exports = {
           DEFAULT: '#fc8383'
         },
         gray: {
-          'gray-1': '#f9fafc',
-          'gray-2': '#eff2f7',
-          'gray-3': '#e5e9f2',
-          'gray-4': '#D3dce6',
+          'gray-3': '#3c4858',
+          'gray-4': '#8492a6',
           'gray-5': '#c2cedB',
-          'gray-6': '#8492a6',
-          'gray-7': '#3c4858'
+          'gray-6': '#D3dce6',
+          'gray-7': '#e5e9f2',
+          'gray-8': '#eff2f7',
+          'gray-9': '#f9fafc'
         },
         states: {
           success: '#37ccb2',
@@ -33,8 +33,14 @@ module.exports = {
         },
         copy: {
           white: '#ffffff',
-          'light-black': '#1f2d3d'
+          'gray-2': '#1f2d3d'
         }
+      },
+      zIndex: {
+        '-1' : '-1',
+      },
+      screens: {
+        'xs': '375px',
       },
     },
   },
