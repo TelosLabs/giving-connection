8962c696c451d8d530b05c219b6834bd28ef80b1
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-16 17:40:54 -0600

    add skip before action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-  before_action :check_auth
 
   def index; end
-
-  def check_auth
-    unless user_signed_in?
-      redirect_to :controller => :landing_page
-    end
-  end
 end
