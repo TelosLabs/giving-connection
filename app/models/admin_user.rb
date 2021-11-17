01b355376d24897b7f1b66d4e49c2ac0dc061635
Pedro Miranda <pbragamiranda@gmail.com>
2021-11-17 17:59:33 -0300

    removing registrable module from admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -24,8 +24,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,:recoverable, :rememberable, :validatable
 
   has_many :organizations, as: :creator
 end
