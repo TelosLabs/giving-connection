6481bc4eb3cb3015e09f25f81847400294097102
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-18 14:21:38 -0600

    rubocop fixes

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
+# Helper module for error messages in forms
 module FormHelper
   def errors_for(form, field)
       content_tag(:p, form.object.errors[field].try(:first), class: 'error block text-states-error')
   end
 
-  def form_group_for(form, field, opts={}, &block)
+  def form_group_for(form, field, opts = {}, &block)
     label = opts.fetch(:label) { true }
     has_errors = form.object.errors[field].present?
 
     content_tag :div, class: "c-form--group #{'has-error' if has_errors}" do
-        if label === true
-          concat form.label(field, class: 'control-label')
-        elsif label
-          concat form.label(label, class: 'control-label')
-        end
-        concat capture(&block)
-        concat errors_for(form, field)
+      if label === true
+        concat form.label(field, class: 'control-label')
+      elsif label
+        concat form.label(label, class: 'control-label')
+      end
+      concat capture(&block)
+      concat errors_for(form, field)
     end
   end
 end
