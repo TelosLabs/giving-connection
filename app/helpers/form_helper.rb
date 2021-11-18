ecb9d76e05a0fae87c9749ed2d234ad57c6bda53
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-18 14:52:43 -0600

    update form error helper

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -6,16 +6,17 @@ module FormHelper
     content_tag(:p, form.object.errors[field].try(:first), class: 'error block text-states-error')
   end
 
+  def errors_label(form, label, field)
+    return form.label(field, class: 'control-label') if label == true
+    return form.label(label, class: 'control-label') if label
+  end
+
   def form_group_for(form, field, opts = {}, &block)
     label = opts.fetch(:label, true)
     has_errors = form.object.errors[field].present?
 
     content_tag :div, class: "c-form--group #{'has-error' if has_errors}" do
-      if label == true
-        concat form.label(field, class: 'control-label')
-      elsif label
-        concat form.label(label, class: 'control-label')
-      end
+      concat errors_label(form, label, field)
       concat capture(&block)
       concat errors_for(form, field)
     end
