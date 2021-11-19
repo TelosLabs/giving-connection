4c8202d85a60d3d37a09ce3c88b095035f050a2c
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-19 14:22:00 -0600

    update options values

--- app/javascript/controllers/contact_form_controller.js
+++ app/javascript/controllers/contact_form_controller.js
@@ -6,14 +6,12 @@ export default class extends Controller {
   }
 
   subjectSelected() {
-    if (this.subjectTarget.value === 'I want to publish a non profit on Giving Connection') {
-      this.messageTarget.value = 'Hi, I’m interested in publishing my non profit on Giving Connection.'
-    }
-    if (this.subjectTarget.value === 'I want to claim ownership of a non profit page') {
-      this.messageTarget.value = 'Hi, I’m interested in claiming a non-profit profile already published on Giving Connection.'
-    }
-    if (this.subjectTarget.value === ('' || 'Other')) {
-      this.messageTarget.value = ''
+    if (this.subjectTarget.value === '1') {
+      this.messageTarget.innerText = 'Hi, I’m interested in publishing my non profit on Giving Connection.'
+    } else if (this.subjectTarget.value === '2') {
+      this.messageTarget.innerText = 'Hi, I’m interested in claiming a non-profit profile already published on Giving Connection.'
+    } else {
+      return this.messageTarget.innerText = ''
     }
   }
 }
