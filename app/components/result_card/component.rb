71e6c7d95884cdbb76d535b9dbd78bc2848dbd99
Jose Fernando Davila <davila.jose23@gmail.com>
2021-11-19 12:21:08 -0600

    Add result card component

---
+++ app/components/result_card/component.rb
@@ -0,0 +1,10 @@
+class ResultCard::Component < ViewComponent::Base
+  def initialize(title:, address:, image_url:, webpage:, description:, id:)
+    @title = title
+    @address = address
+    @image_url = image_url
+    @webpage = webpage
+    @id = id
+    @description = description
+  end
+end
