b690e65691dd304cab4af653651e70565cc00869
Jose Fernando Davila <davila.jose23@gmail.com>
2021-11-19 12:23:00 -0600

    Update stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,19 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+const contextComponents = require.context("../../components", true, /_controller\.js$/)
+application.load(
+  definitionsFromContext(context).concat(
+    definitionsFromContext(contextComponents)
+  )
+)
+
+import { Tabs, Modal, Dropdown } from "tailwindcss-stimulus-components"
+application.register('tabs', Tabs)
+application.register('modal', Modal)
+application.register('dropdown', Dropdown)
