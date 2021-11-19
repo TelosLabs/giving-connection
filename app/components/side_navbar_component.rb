62d6afe0b3cabb1752391aeb9b4a5a430c9d757d
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-18 20:18:44 -0600

    move sidebar component into navbar

--- app/components/side_navbar_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# sidebar view component
-class SideNavbarComponent < ViewComponent::Base
-  def initialize(signed_in:)
-    super
-    @signed_in = signed_in
-  end
-end
