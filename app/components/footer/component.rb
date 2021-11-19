d1618dc673de5ef7a271ea253f2fc6c828d96104
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-18 20:40:16 -0600

    rubocop fix

--- app/components/footer/component.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-class Footer::Component < ViewComponent::Base; end
---
+++ app/components/footer/footer_component.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+class Footer::FooterComponent < ViewComponent::Base; end
