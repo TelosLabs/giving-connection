e0f9cadf0fea6265ba0eb910612de69bc322c3a5
evertsalinas <evertsalinas88@gmail.com>
2021-11-20 17:40:18 -0600

    major refactoring to search experience

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,20 +7,19 @@ class SearchesController < ApplicationController
 
   def create
     search = Search.new(create_params)
-    if search.search && search.results.any?
+
+    if search.save && search.results.any?
       @results = search.results
       redirect_to locations_path(ids: @results.ids)
     else
-      puts 'didnt work'
+      render :new
+      puts search.errors.full_messages
     end
-  end
-
-  def keyword_params
-    params.permit(:keyword)
   end
 
   def create_params
     params.permit(:distance, :city, :state, :beneficiary_groups,
-                  :services, :open_now, :open_weekends, :keyword)
+                  :services, :open_now, :open_weekends, :keyword,
+                  :search_type)
   end
 end
--- app/models/locations/searchable.rb
+++ app/models/locations/searchable.rb
@@ -20,14 +20,5 @@ module Locations
                         tsearch: { prefix: true, any_word: true }
                       }
     end
-
-    class_methods do
-      def geo_near(coords, distance)
-        where(
-          'ST_DWithin(lonlat, :point, :distance)',
-          { point: coords, distance: distance * 1000 } # wants meters not kms
-        )
-      end
-    end
   end
 end
