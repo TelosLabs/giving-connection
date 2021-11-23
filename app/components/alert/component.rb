7c1590afeae45bdd435defbbd1ce47a52c29aa06
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-23 16:46:20 -0600

    rubocop fixes

--- app/components/alert/component.rb
+++ app/components/alert/component.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/ClassAndModuleChildren
+# rubocop:disable Lint/MissingSuper
+
+# View Component to display flash alerts
 class Alert::Component < ViewComponent::Base
   def initialize(key:, message:)
     @key = key
@@ -7,27 +11,30 @@ class Alert::Component < ViewComponent::Base
   end
 
   def background
-    return 'bg-green-50' if @key === 'notice'
-    return 'bg-red-50' if @key === 'alert'
+    return 'bg-green-50' if @key == 'notice'
+    return 'bg-red-50' if @key == 'alert'
   end
 
   def notification_icon
-    return 'notice-icon.svg' if @key === 'notice'
-    return 'alert-icon.svg' if @key === 'alert'
+    return 'notice-icon.svg' if @key == 'notice'
+    return 'alert-icon.svg' if @key == 'alert'
   end
 
   def notification_icon_classes
-    return 'text-green-400' if @key === 'notice'
-    return 'text-red-400' if @key === 'alert'
+    return 'text-green-400' if @key == 'notice'
+    return 'text-red-400' if @key == 'alert'
   end
 
   def notification_text_classes
-    return 'text-green-800' if @key === 'notice'
-    return 'text-red-800' if @key === 'alert'
+    return 'text-green-800' if @key == 'notice'
+    return 'text-red-800' if @key == 'alert'
   end
 
   def button_classes
-    return 'bg-green-50 text-green-500 hover:bg-green-100 focus:ring-offset-green-50 focus:ring-green-600' if @key === 'notice'
-    return 'bg-red-50 text-red-500 hover:bg-red-100 focus:ring-offset-red-50 focus:ring-red-600' if @key === 'alert'
+    return 'bg-green-50 text-green-500 hover:bg-green-100 focus:ring-offset-green-50 focus:ring-green-600' if @key == 'notice'
+    return 'bg-red-50 text-red-500 hover:bg-red-100 focus:ring-offset-red-50 focus:ring-red-600' if @key == 'alert'
   end
 end
+
+# rubocop:enable Style/ClassAndModuleChildren
+# rubocop:enable Lint/MissingSuper
