fe89704f01ed4f52679a10e034c1c54cc7c2db17
Pedro Miranda <pbragamiranda@gmail.com>
2021-11-24 19:54:02 +0100

    adding lat and long

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,175 +1,175 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# # Admin Users
-# AdminUser.create!(email: 'admin@example.com', password: 'testing', password_confirmation: 'testing')
+# Admin Users
+AdminUser.create!(email: 'admin@example.com', password: 'testing', password_confirmation: 'testing')
 
-# # Users
-# User.create!(email: 'user@example.com', password: 'testing', password_confirmation: 'testing')
+# Users
+User.create!(email: 'user@example.com', password: 'testing', password_confirmation: 'testing')
 
-# # Orgs
-# org = Organization.new(
-#   name: 'Centerstone Clarksville',
-#   ein_number: rand(0..1000),
-#   irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
-#   website: 'centerstone@example.com',
-#   scope_of_work: %w[International National Regional].sample,
-#   mission_statement_en: Faker::Company.catch_phrase,
-#   vision_statement_en: Faker::Company.catch_phrase,
-#   tagline_en: Faker::Company.catch_phrase,
-#   description_en: Faker::Company.catch_phrase
-# )
-# org.creator = AdminUser.last
+# Orgs
+org = Organization.new(
+  name: 'Centerstone Clarksville',
+  ein_number: rand(0..1000),
+  irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
+  website: 'centerstone@example.com',
+  scope_of_work: %w[International National Regional].sample,
+  mission_statement_en: Faker::Company.catch_phrase,
+  vision_statement_en: Faker::Company.catch_phrase,
+  tagline_en: Faker::Company.catch_phrase,
+  description_en: Faker::Company.catch_phrase
+)
+org.creator = AdminUser.last
 
-# org.locations.build(
-#   lonlat: Geo.point(-87.3499228477923, 36.53588038686436),
-#   longitude: '-87.3499228477923',
-#   latitude: '36.53588038686436',
-#   address: 'Centerstone, 8th Street, Clarksville, TN, USA',
-#   main: true,
-#   offer_services: true,
-#   physical: true,
-# )
+org.locations.build(
+  lonlat: Geo.point(-87.3499228477923, 36.53588038686436),
+  longitude: '-87.3499228477923',
+  latitude: '36.53588038686436',
+  address: 'Centerstone, 8th Street, Clarksville, TN, USA',
+  main: true,
+  offer_services: true,
+  physical: true,
+)
 
-# Date::DAYNAMES.each_with_index do |day, index|
-#   org.locations.first.office_hours.build(
-#     day: index,
-#     open_time: Time.now.change({ hour: "9:00" }),
-#     close_time: Time.now.change({ hour: "16:00" }),
-#     closed: ["Saturday", "Sunday"].include?(day)
-#   )
-# end
+Date::DAYNAMES.each_with_index do |day, index|
+  org.locations.first.office_hours.build(
+    day: index,
+    open_time: Time.now.change({ hour: "9:00" }),
+    close_time: Time.now.change({ hour: "16:00" }),
+    closed: ["Saturday", "Sunday"].include?(day)
+  )
+end
 
-# puts "Org with id #{org.id} sucessfully created" if org.save!
+puts "Org with id #{org.id} sucessfully created" if org.save!
 
-# org = Organization.new(
-#   name: 'NAMI Tennessee',
-#   ein_number: rand(0..1000),
-#   irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
-#   website: 'nami@example.com',
-#   scope_of_work: %w[International National Regional].sample,
-#   mission_statement_en: Faker::Company.catch_phrase,
-#   vision_statement_en: Faker::Company.catch_phrase,
-#   tagline_en: Faker::Company.catch_phrase,
-#   description_en: Faker::Company.catch_phrase
-# )
-# org.creator = AdminUser.last
+org = Organization.new(
+  name: 'NAMI Tennessee',
+  ein_number: rand(0..1000),
+  irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
+  website: 'nami@example.com',
+  scope_of_work: %w[International National Regional].sample,
+  mission_statement_en: Faker::Company.catch_phrase,
+  vision_statement_en: Faker::Company.catch_phrase,
+  tagline_en: Faker::Company.catch_phrase,
+  description_en: Faker::Company.catch_phrase
+)
+org.creator = AdminUser.last
 
-# org.locations.build(
-#   lonlat: Geo.point(-86.70336982081703, 36.12308987261626),
-#   longitude: '-86.70336982081703',
-#   latitude: '36.12308987261626',
-#   address: 'NAMI Tennessee, Kermit Drive, Nashville, Tennessee, USA',
-#   main: true,
-#   offer_services: true,
-#   physical: true,
-# )
+org.locations.build(
+  lonlat: Geo.point(-86.70336982081703, 36.12308987261626),
+  longitude: '-86.70336982081703',
+  latitude: '36.12308987261626',
+  address: 'NAMI Tennessee, Kermit Drive, Nashville, Tennessee, USA',
+  main: true,
+  offer_services: true,
+  physical: true,
+)
 
-# Date::DAYNAMES.each_with_index do |day, index|
-#   org.locations.first.office_hours.build(
-#     day: index,
-#     open_time: Time.now.change({ hour: "9:00" }),
-#     close_time: Time.now.change({ hour: "16:00" }),
-#     closed: false
-#   )
-# end
+Date::DAYNAMES.each_with_index do |day, index|
+  org.locations.first.office_hours.build(
+    day: index,
+    open_time: Time.now.change({ hour: "9:00" }),
+    close_time: Time.now.change({ hour: "16:00" }),
+    closed: false
+  )
+end
 
-# puts "Org with id #{org.id} sucessfully created" if org.save!
+puts "Org with id #{org.id} sucessfully created" if org.save!
 
-# org = Organization.new(
-#   name: 'Mental Health America of the MidSouth',
-#   ein_number: rand(0..1000),
-#   irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
-#   website: 'mental@example.com',
-#   scope_of_work: %w[International National Regional].sample,
-#   mission_statement_en: Faker::Company.catch_phrase,
-#   vision_statement_en: Faker::Company.catch_phrase,
-#   tagline_en: Faker::Company.catch_phrase,
-#   description_en: Faker::Company.catch_phrase
-# )
-# org.creator = AdminUser.last
+org = Organization.new(
+  name: 'Mental Health America of the MidSouth',
+  ein_number: rand(0..1000),
+  irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
+  website: 'mental@example.com',
+  scope_of_work: %w[International National Regional].sample,
+  mission_statement_en: Faker::Company.catch_phrase,
+  vision_statement_en: Faker::Company.catch_phrase,
+  tagline_en: Faker::Company.catch_phrase,
+  description_en: Faker::Company.catch_phrase
+)
+org.creator = AdminUser.last
 
-# org.locations.build(
-#   lonlat: Geo.point(-86.69885745944305, 36.08984574780193),
-#   longitude: '-86.69885745944305',
-#   latitude: '36.08984574780193',
-#   address: 'Mental Health America of the MidSouth, Nashville, TN, USA',
-#   main: true,
-#   offer_services: true,
-#   physical: true,
-# )
+org.locations.build(
+  lonlat: Geo.point(-86.69885745944305, 36.08984574780193),
+  longitude: '-86.69885745944305',
+  latitude: '36.08984574780193',
+  address: 'Mental Health America of the MidSouth, Nashville, TN, USA',
+  main: true,
+  offer_services: true,
+  physical: true,
+)
 
-# Date::DAYNAMES.each_with_index do |day, index|
-#   org.locations.first.office_hours.build(
-#     day: index,
-#     open_time: Time.now.change({ hour: "9:00" }),
-#     close_time: Time.now.change({ hour: "16:00" }),
-#     closed: false
-#   )
-# end
+Date::DAYNAMES.each_with_index do |day, index|
+  org.locations.first.office_hours.build(
+    day: index,
+    open_time: Time.now.change({ hour: "9:00" }),
+    close_time: Time.now.change({ hour: "16:00" }),
+    closed: false
+  )
+end
 
-# puts "Org with id #{org.id} sucessfully created" if org.save!
+puts "Org with id #{org.id} sucessfully created" if org.save!
 
-# org = Organization.new(
-#   name: 'System of Care Across Tennessee',
-#   ein_number: rand(0..1000),
-#   irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
-#   website: 'systemofcare@example.com',
-#   scope_of_work: %w[International National Regional].sample,
-#   mission_statement_en: Faker::Company.catch_phrase,
-#   vision_statement_en: Faker::Company.catch_phrase,
-#   tagline_en: Faker::Company.catch_phrase,
-#   description_en: Faker::Company.catch_phrase
-# )
-# org.creator = AdminUser.last
+org = Organization.new(
+  name: 'System of Care Across Tennessee',
+  ein_number: rand(0..1000),
+  irs_ntee_code: %w[A00 A90 A26 A91 A02 Q21].sample,
+  website: 'systemofcare@example.com',
+  scope_of_work: %w[International National Regional].sample,
+  mission_statement_en: Faker::Company.catch_phrase,
+  vision_statement_en: Faker::Company.catch_phrase,
+  tagline_en: Faker::Company.catch_phrase,
+  description_en: Faker::Company.catch_phrase
+)
+org.creator = AdminUser.last
 
-# org.locations.build(
-#   lonlat: Geo.point(-86.78218038642588, 36.16560224965609),
-#   longitude: '-86.78218038642588',
-#   latitude: '36.16560224965609',
-#   address: 'System of Care Across Tennessee',
-#   main: true,
-#   offer_services: true,
-#   physical: true,
-# )
+org.locations.build(
+  lonlat: Geo.point(-86.78218038642588, 36.16560224965609),
+  longitude: '-86.78218038642588',
+  latitude: '36.16560224965609',
+  address: 'System of Care Across Tennessee',
+  main: true,
+  offer_services: true,
+  physical: true,
+)
 
-# Date::DAYNAMES.each_with_index do |day, index|
-#   org.locations.first.office_hours.build(
-#     day: index,
-#     open_time: Time.now.change({ hour: "9:00" }),
-#     close_time: Time.now.change({ hour: "16:00" }),
-#     closed: ["Saturday", "Sunday"].include?(day)
-#   )
-# end
+Date::DAYNAMES.each_with_index do |day, index|
+  org.locations.first.office_hours.build(
+    day: index,
+    open_time: Time.now.change({ hour: "9:00" }),
+    close_time: Time.now.change({ hour: "16:00" }),
+    closed: ["Saturday", "Sunday"].include?(day)
+  )
+end
 
-# puts "Org with id #{org.id} sucessfully created" if org.save!
-
-
-# # Beneficiaries
-# Organizations::Constants::BENEFICIARIES.each do |beneficiary, subbeneficiaries|
-#   new_beneficiary = BeneficiaryGroup.new(name: beneficiary)
-
-#   puts "#{new_beneficiary.name} sucessfully created" if new_beneficiary.save!
-
-#   subbeneficiaries.each do |subbeneficiary|
-#     new_subbeneficiary = BeneficiarySubcategory.new(name: subbeneficiary, beneficiary_group: new_beneficiary)
-
-#     puts "#{new_subbeneficiary.name} sucessfully created" if new_subbeneficiary.save!
-#   end
-# end
+puts "Org with id #{org.id} sucessfully created" if org.save!
 
 
-# # Causes and Services
-# Organizations::Constants::CAUSES_AND_SERVICES.each do |cause, services|
-#   new_cause = Cause.new(name: cause)
+# Beneficiaries
+Organizations::Constants::BENEFICIARIES.each do |beneficiary, subbeneficiaries|
+  new_beneficiary = BeneficiaryGroup.new(name: beneficiary)
 
-#   puts "#{new_cause.name} sucessfully created" if new_cause.save!
+  puts "#{new_beneficiary.name} sucessfully created" if new_beneficiary.save!
 
-#   services.each do |service|
-#     new_service = Service.new(name: service, cause: new_cause)
+  subbeneficiaries.each do |subbeneficiary|
+    new_subbeneficiary = BeneficiarySubcategory.new(name: subbeneficiary, beneficiary_group: new_beneficiary)
 
-#     puts "#{new_service.name} sucessfully created" if new_service.save!
-#   end
-# end
+    puts "#{new_subbeneficiary.name} sucessfully created" if new_subbeneficiary.save!
+  end
+end
+
+
+# Causes and Services
+Organizations::Constants::CAUSES_AND_SERVICES.each do |cause, services|
+  new_cause = Cause.new(name: cause)
+
+  puts "#{new_cause.name} sucessfully created" if new_cause.save!
+
+  services.each do |service|
+    new_service = Service.new(name: service, cause: new_cause)
+
+    puts "#{new_service.name} sucessfully created" if new_service.save!
+  end
+end
 
 file_path = File.open(File.join(Rails.root, 'db', 'uploads'))
 
@@ -235,14 +235,15 @@ CSV.foreach(orgs_csv_file, headers: :first_row) do |org_row|
       new_location = Location.create!(address: location_row['address'], website: location_row['website'],
                                   main: location_row['main'] == 'yes', physical: location_row['physical'] == 'yes',
                                   appointment_only: location_row['appointment_only'] == 'yes',
-                                  offer_services: location_row['offer_services'] == 'yes',
-                                  organization: new_organization )
+                                  offer_services: location_row['offer_services'] == 'yes', organization: new_organization, 
+                                  latitude: location_row['latitude'].to_i, longitude: location_row['longitude'].to_i)
     end
 
 
     CSV.foreach(location_services_csv_file, headers: :first_row) do |location_service_row|
       if location_row['id'] == location_service_row['location_id']
-        new_location_service = LocationService.create!(name: location_service_row['name'], location: new_location)
+        service = Service.find_by_name(location_service_row['name'])
+        new_location_service = LocationService.create!(service: service, location: new_location)
       end
     end
 
