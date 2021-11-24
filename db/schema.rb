29e5566bb7966ae1f02ed9f4624d13e14f33e18e
Pedro Miranda <pbragamiranda@gmail.com>
2021-11-24 15:53:11 +0100

    adding role to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_202634) do
+ActiveRecord::Schema.define(version: 2021_11_24_144405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,22 @@ ActiveRecord::Schema.define(version: 2021_11_08_202634) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "alerts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "distance"
+    t.string "city"
+    t.string "state"
+    t.string "services"
+    t.string "open_now"
+    t.string "open_weekends"
+    t.string "keyword"
+    t.string "beneficiary_groups"
+    t.string "frequency", default: "weekly"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_alerts_on_user_id"
+  end
+
   create_table "beneficiary_groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -90,6 +106,15 @@ ActiveRecord::Schema.define(version: 2021_11_08_202634) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "favorite_locations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "location_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["location_id"], name: "index_favorite_locations_on_location_id"
+    t.index ["user_id"], name: "index_favorite_locations_on_user_id"
+  end
+
   create_table "location_services", force: :cascade do |t|
     t.string "description"
     t.bigint "location_id", null: false
@@ -112,12 +137,13 @@ ActiveRecord::Schema.define(version: 2021_11_08_202634) do
     t.bigint "organization_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "appointment_only", default: false
     t.index ["lonlat"], name: "index_locations_on_lonlat", using: :gist
     t.index ["organization_id"], name: "index_locations_on_organization_id"
   end
 
   create_table "office_hours", force: :cascade do |t|
-    t.string "day", null: false
+    t.integer "day", null: false
     t.time "open_time"
     t.time "close_time"
     t.boolean "closed", default: false
@@ -208,6 +234,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_202634) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name"
     t.index ["organization_id"], name: "index_tags_on_organization_id"
   end
 
@@ -231,6 +258,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_202634) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -239,7 +267,10 @@ ActiveRecord::Schema.define(version: 2021_11_08_202634) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "alerts", "users"
   add_foreign_key "beneficiary_subcategories", "beneficiary_groups"
+  add_foreign_key "favorite_locations", "locations"
+  add_foreign_key "favorite_locations", "users"
   add_foreign_key "location_services", "locations"
   add_foreign_key "location_services", "services"
   add_foreign_key "locations", "organizations"
