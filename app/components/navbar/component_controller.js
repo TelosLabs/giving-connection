d82df03ad756215b8416e5c2870ae2113df29e44
Luis Saavedra <luis.saavedra@teloslabs.co>
2021-11-23 18:06:55 -0600

    disable scroll when sidebar is displayed

--- app/components/navbar/component_controller.js
+++ app/components/navbar/component_controller.js
@@ -9,11 +9,13 @@ export default class extends Controller {
     this.sideNavBarTarget.classList.remove('hidden')
     this.sideNavBarTarget.classList.add('sidebar-slide-in')
     this.sideNavBarTarget.classList.remove('sidebar-slide-out')
+    document.body.classList.add('overflow-hidden')
   }
 
   collapseSideNavBar() {
     this.sideNavBarTarget.classList.remove('sidebar-slide-in')
     this.sideNavBarTarget.classList.add('sidebar-slide-out')
+    document.body.classList.remove('overflow-hidden')
     setTimeout(() => {
       this.sideNavBarTarget.classList.add('hidden')
     }, 500);
