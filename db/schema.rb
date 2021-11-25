75675913522a7e96733647f71203d0fdc2e90d10
Jose Fernando Davila <davila.jose23@gmail.com>
2021-11-25 17:26:18 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,12 @@ ActiveRecord::Schema.define(version: 2021_11_25_171346) do
     t.index ["beneficiary_group_id"], name: "index_beneficiary_subcategories_on_beneficiary_group_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "causes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -134,7 +140,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_171346) do
   end
 
   create_table "office_hours", force: :cascade do |t|
-    t.integer "day", null: false
+    t.string "day", null: false
     t.time "open_time"
     t.time "close_time"
     t.boolean "closed", default: false
@@ -151,6 +157,15 @@ ActiveRecord::Schema.define(version: 2021_11_25_171346) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["beneficiary_subcategory_id"], name: "index_organization_beneficiaries_on_beneficiary_subcategory_id"
     t.index ["organization_id"], name: "index_organization_beneficiaries_on_organization_id"
+  end
+
+  create_table "organization_categories", force: :cascade do |t|
+    t.bigint "organization_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_organization_categories_on_category_id"
+    t.index ["organization_id"], name: "index_organization_categories_on_organization_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -216,7 +231,6 @@ ActiveRecord::Schema.define(version: 2021_11_25_171346) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_tags_on_name"
     t.index ["organization_id"], name: "index_tags_on_organization_id"
   end
 
@@ -256,6 +270,8 @@ ActiveRecord::Schema.define(version: 2021_11_25_171346) do
   add_foreign_key "locations", "organizations"
   add_foreign_key "organization_beneficiaries", "beneficiary_subcategories"
   add_foreign_key "organization_beneficiaries", "organizations"
+  add_foreign_key "organization_categories", "categories"
+  add_foreign_key "organization_categories", "organizations"
   add_foreign_key "services", "causes"
   add_foreign_key "social_medias", "organizations"
   add_foreign_key "tags", "organizations"
