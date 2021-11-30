6f928929d307f24d42705ed2dab9587828b5a7f4
evertsalinas <evertsalinas88@gmail.com>
2021-11-30 12:48:04 -0600

    Account management

--- app/controllers/my_accounts_controller.rb
+++ app/controllers/my_accounts_controller.rb
@@ -1,5 +1,9 @@
 class MyAccountsController < ApplicationController
 
   def show
+    # @user
+    @saved_pages = current_user.favorited_locations
+    @alerts = current_user.alerts
+    # @my_pages
   end
 end
