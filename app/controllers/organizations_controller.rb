ecf4bea4d7d5c8dfa4e482905aca065e885a59ab
Pedro Miranda <pbragamiranda@gmail.com>
2021-12-01 00:52:03 +0100

    adding tags to form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -16,7 +16,7 @@ class OrganizationsController < ApplicationController
     authorize @organization
     if @organization.update(organization_params)
       # update_organization_beneficiaries(requested_resource, resource_params['beneficiary_subcategories_id']) unless resource_params['beneficiary_subcategories_id'].nil?
-      # update_tags(requested_resource, JSON.parse(params['tags_attributes'])) unless params['tags_attributes'].strip.empty?
+      update_tags(@organization, JSON.parse(params['tags_attributes'])) unless params['tags_attributes'].strip.empty?
       redirect_to organization_path(@organization)
     else
       render :edit
@@ -28,33 +28,33 @@ class OrganizationsController < ApplicationController
           .permit(:name, :ein_number, :irs_ntee_code, :website, :scope_of_work,
                   :mission_statement_en, :mission_statement_es, :vision_statement_en,
                   :vision_statement_es, :tagline_en, :tagline_es, :email, :phone_number,
-                  social_media_attributes: %i[facebook instagram twitter linkedin youtube blog id])            
-    # tags_attributes: [])
+                  social_media_attributes: %i[facebook instagram twitter linkedin youtube blog id],            
+                  tags_attributes: [],
+                  location_attributes: %i[address latitude longitude website main physical offer_services appointment_only])
     # service_attributes: %i[name description id],
     # beneficiary_subcategories_id: [],
     # services_id: [],
-    # main_location_attributes: %i[address latitude longitude website main physical offer_services],
   end
 
-  # def create_tags(organization, tags)
-  #   tags.each do |tag_hash|
-  #     Tag.create!(organization: organization, name: tag_hash['value'])
-  #   end
-  # end
+  def create_tags(organization, tags)
+    tags.each do |tag_hash|
+      Tag.create!(organization: organization, name: tag_hash['value'])
+    end
+  end
 
-  # def update_tags(organization, tags)
-  #   to_create = tags - organization.tags.map(&:name)
-  #   to_delete = organization.tags.map(&:name) - tags
+  def update_tags(organization, tags)
+    to_create = tags - organization.tags.map(&:name)
+    to_delete = organization.tags.map(&:name) - tags
 
-  #   create_tags(organization, to_create)
-  #   delete_tags(organization, to_delete)
-  # end
+    create_tags(organization, to_create)
+    delete_tags(organization, to_delete)
+  end
 
-  # def delete_tags(organization, tags)
-  #   tags.each do |tag_name|
-  #     organization.tags.find_by(name: tag_name).delete
-  #   end
-  # end
+  def delete_tags(organization, tags)
+    tags.each do |tag_name|
+      organization.tags.find_by(name: tag_name).delete
+    end
+  end
 end
 
  # JSON.parse(params['tags_attributes'])
