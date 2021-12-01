c6209f2dfe6bf0c8bc34c22241ae1216ff591761
Jose Fernando Davila <davila.jose23@gmail.com>
2021-12-01 11:13:41 -0600

    Delte connect method

--- app/javascript/controllers/options_controller.js
+++ app/javascript/controllers/options_controller.js
@@ -4,10 +4,6 @@ import Tagify from '@yaireo/tagify';
 export default class extends Controller {
   static target = [ "list" ]
 
-  connect() {
-    console.log('connected')
-
-  }
 
   listTargetConnected(target) {
     new Tagify(
