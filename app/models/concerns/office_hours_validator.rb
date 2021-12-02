71b58b016488946b9eec7d18cffa4f56de294e7e
evertsalinas <evertsalinas88@gmail.com>
2021-12-02 14:26:19 -0600

    Minor fix

--- app/models/concerns/office_hours_validator.rb
+++ app/models/concerns/office_hours_validator.rb
@@ -11,7 +11,7 @@ class OfficeHoursValidator < ActiveModel::Validator
   def close_hour_after_open_hour
     return true if record.closed?
 
-    if record.open_time >= record.close_time
+    if record&.open_time >= record&.close_time
       record.organization.errors.add(:base, 'Closing time must be after opening time')
     end
   end
