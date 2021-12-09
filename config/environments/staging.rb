c0ceccf040d85cdbef9964d7016f7fcb84c96460
evertsalinas <evertsalinas88@gmail.com>
2021-12-09 11:28:42 -0600

    Fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,8 +68,6 @@ Rails.application.configure do
 
   config.action_mailer.default charset: 'utf-8'
 
-  config.action_mailer.asset_host = 'https://givingconnection-staging.herokuapp.com/'
-
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
