6ba470c8e56b96703664ceae1d15ccf9da6c5a32
evertsalinas <evertsalinas88@gmail.com>
2021-12-08 23:35:45 -0600

    update annotation

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: phone_numbers
+#
+#  id          :bigint           not null, primary key
+#  number      :string
+#  main        :boolean
+#  location_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class PhoneNumber < ApplicationRecord
   belongs_to :location
 end
--- spec/factories/phone_numbers.rb
+++ spec/factories/phone_numbers.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: phone_numbers
+#
+#  id          :bigint           not null, primary key
+#  number      :string
+#  main        :boolean
+#  location_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :phone_number do
     number { "MyString" }
