171be8f1c2d6bc3e1f013733ac06fa22ec37afde
evertsalinas <evertsalinas88@gmail.com>
2022-01-02 15:07:58 -0600

    Minor fix

--- app/controllers/my_accounts_controller.rb
+++ app/controllers/my_accounts_controller.rb
@@ -7,6 +7,6 @@ class MyAccountsController < ApplicationController
   def show
     @saved_pages = current_user.favorited_locations
     @alerts = current_user.alerts
-    @my_organizations = current_user.organizations
+    @my_organizations = current_user.administrated_organizations
   end
 end
