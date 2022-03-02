1fc94f979b0eec974fc3a854dffd30770392a9b7
Jose Fernando Davila <davila.jose23@gmail.com>
2022-03-01 23:13:27 -0600

    Add terms and policies

---
+++ app/controllers/terms_and_conditions_controller.rb
@@ -0,0 +1,10 @@
+class TermsAndConditionsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  include Pundit
+
+  skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
+
+  def show; end
+end
