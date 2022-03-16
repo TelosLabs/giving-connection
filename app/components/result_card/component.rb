4387d965a403f723c39b5678505c4d36fdc09154
Pedro Miranda <pbragamiranda@gmail.com>
2022-03-16 09:55:19 +0100

    adding click-to-call to phone numbers

--- app/components/result_card/component.rb
+++ app/components/result_card/component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ResultCard::Component < ViewComponent::Base
-  def initialize(title:, address:, image_url:, website:, description:, id:, current_user: )
+  def initialize(title:, address:, image_url:, website:, description:, id:, current_user:, phone_number: )
     @title = title
     @address = address
     @image_url = image_url
@@ -8,6 +8,7 @@ class ResultCard::Component < ViewComponent::Base
     @id = id
     @description = description
     @current_user = current_user
+    @phone_number = phone_number
   end
 
   def website_url
@@ -26,5 +27,12 @@ class ResultCard::Component < ViewComponent::Base
 
   def formated_description
     @description.length <= 280 ? @description : "#{@description[0..280]} (...)"
+  end
+
+  def device
+    agent = request.user_agent
+    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+    return "mobile" if agent =~ /Mobile/
+    return "desktop"
   end
 end
