3b7b88c8764c5767977855222d9ba2c6eb1b52d8
Pedro Miranda <pbragamiranda@gmail.com>
2022-04-18 21:45:42 +0200

    adding more fields to org form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -80,7 +80,7 @@ class OrganizationsController < ApplicationController
                   social_media_attributes: %i[facebook instagram twitter linkedin youtube blog id],
                   tags_attributes: [],
                   locations_attributes: [:id, :name, :address, :latitude, :longitude, :website, :po_box,
-                                         :main, :physical, :offer_services, :appointment_only, :_destroy, 
+                                         :main, :physical, :offer_services, :appointment_only, :email, :_destroy, 
                                          { phone_number_attributes: [:number],
                                            office_hours_attributes: %i[id day open_time close_time closed] }],
                   beneficiary_subcategories: [])
