09bf6171c8b3e0465895e843caae6c9707c8b05b
Alan Soto <soto.alan@hotmail.com>
2022-05-16 17:10:14 -0500

    Call attachment method in before_action

--- app/mailers/saved_search_alert_mailer.rb
+++ app/mailers/saved_search_alert_mailer.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class SavedSearchAlertMailer < ApplicationMailer
+  before_action :attach_gc_logo, only: :send_alert
+
   def send_alert(alert)
     @alert = alert
     search = Search.new(search_params(alert))
     search.save
-    attach_gc_logo
-    attach_organizations_logos
     @alert_filters = build_alert_filters
     @new_locations = search.results.select {|result| result.created_at > alert.next_alert || result.updated_at > alert.next_alert}
     unless @new_locations.empty?
-       mail from: 'Giving Connection <notification@teloslabs.co>', to: alert.user.email, subject: "Giving Connection - #{@new_locations.count} New Locations Added !"
+      attach_organizations_logos
+      mail from: 'Giving Connection <notification@teloslabs.co>', to: alert.user.email, subject: "Giving Connection - #{@new_locations.count} New Locations Added !"
     end
   end
 
@@ -65,7 +66,7 @@ class SavedSearchAlertMailer < ApplicationMailer
   end
 
   def attach_organizations_logos
-    @new_locations.each do |location|
+    @new_locations&.each do |location|
       logo = location.organization.logo
       attachments.inline[logo.blob.filename.to_s] = { mime_type: logo.blob.content_type, content: logo.blob.download }
     end
