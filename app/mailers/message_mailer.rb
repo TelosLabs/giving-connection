465b054245c878c46df95c040c553f6a249f2067
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-20 12:23:29 -0500

    preview added

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,12 +5,12 @@ class MessageMailer < ApplicationMailer
           from: 'Giving Connection <info@givingconnection.org>'
 
   def default_response(message)
-    @message = message
+    @message = params[:message] || message
     mail to: @message.email, subject: 'We received your message!'
   end
 
   def admins_notification(message)
-    @message = message
+    @message = params[:message] || message
     mail subject: 'Contact notification'
   end
 end
---
+++ spec/mailers/previews/message_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/order_mailer
+class MessageMailerPreview < ActionMailer::Preview
+  def default_response
+    message = Message.new(name: "Daniel", email: "daniel.enm17@gmail.com", phone:"5521529085", subject:"Hello, this is subject", organization_name:"",
+      organization_website:"", organization_ein:"", content:"Hello, this is content")
+
+    MessageMailer.with(message: message).default_response
+  end
+end
