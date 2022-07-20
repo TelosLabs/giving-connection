80867404d4ace012af3c6c2a237dbf26c1e5477a
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-20 15:48:11 -0500

    logos attachments

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -6,11 +6,20 @@ class MessageMailer < ApplicationMailer
 
   def default_response(message)
     @message = message
+    attach_logos
     mail to: @message.email, subject: 'We received your message!'
   end
 
   def admins_notification(message)
     @message = message
     mail subject: 'Contact notification'
+  end
+
+  def attach_logos
+    attachments.inline["white_logo.jpg"] = File.read("#{Rails.root}/app/assets/images/white_logo.jpg")
+    attachments.inline["facebook.jpg"] = File.read("#{Rails.root}/app/assets/images/facebook.jpg")
+    attachments.inline["instagram.jpg"] = File.read("#{Rails.root}/app/assets/images/instagram.jpg")
+    attachments.inline["twitter.jpg"] = File.read("#{Rails.root}/app/assets/images/twitter.jpg")
+    attachments.inline["youtoube.jpg"] = File.read("#{Rails.root}/app/assets/images/youtoube.jpg")
   end
 end
