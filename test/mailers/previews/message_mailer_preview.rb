4f637a7e004d993ca5b472010f805326b968c1ec
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-20 12:25:40 -0500

    preview added

--- test/mailers/previews/message_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-class MessageMailerPreview < ActionMailer::Preview
-  def default_response
-    message = Message.new(name: "Daniel", email: "daniel.enm17@gmail.com", phone:"5521529085", subject:"Hello, this is subject", organization_name:,
-      organization_website:, organization_ein:, content:"Hello, this is content")
-
-    MessageMailer.with(message: message).default_response
-  end
-end
