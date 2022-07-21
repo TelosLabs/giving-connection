11de639468543f7e92c51edeaf05583431f02b55
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-21 15:29:29 -0500

    methdos change order

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'Giving Connection <info@givingconnection.org>'
   layout 'mailer'
+  before_action :attach_logos
+
+  private
+
+  def attach_logos
+    attachments.inline["giving_connection_logo.png"] = File.read("#{Rails.root}/app/assets/images/send_alert_logo.png")
+    attachments.inline["facebook.jpg"] = File.read("#{Rails.root}/app/assets/images/facebook.jpg")
+    attachments.inline["instagram.jpg"] = File.read("#{Rails.root}/app/assets/images/instagram.jpg")
+    attachments.inline["twitter.jpg"] = File.read("#{Rails.root}/app/assets/images/twitter.jpg")
+    attachments.inline["youtoube.jpg"] = File.read("#{Rails.root}/app/assets/images/youtoube.jpg")
+  end
 end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,7 +2,7 @@
 
 class MessageMailer < ApplicationMailer
   default to: -> { AdminUser.pluck(:email) },
-          from: Rails.application.credentials.dig(:mailer, :from)
+          from: Rails.application.credentials.dig(:mailchimp, :username)
 
   def default_response(message)
     @message = message
@@ -13,13 +13,5 @@ class MessageMailer < ApplicationMailer
   def admins_notification(message)
     @message = message
     mail subject: 'Contact notification'
-  end
-
-  def attach_logos
-    attachments.inline["giving_connection_logo.png"] = File.read("#{Rails.root}/app/assets/images/send_alert_logo.png")
-    attachments.inline["facebook.jpg"] = File.read("#{Rails.root}/app/assets/images/facebook.jpg")
-    attachments.inline["instagram.jpg"] = File.read("#{Rails.root}/app/assets/images/instagram.jpg")
-    attachments.inline["twitter.jpg"] = File.read("#{Rails.root}/app/assets/images/twitter.jpg")
-    attachments.inline["youtoube.jpg"] = File.read("#{Rails.root}/app/assets/images/youtoube.jpg")
   end
 end
