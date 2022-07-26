6e1447fd48be88db82b8cb2b97f88ed961c1756d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-26 13:55:03 -0500

    Auth working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,12 +24,12 @@ class ApplicationController < ActionController::Base
   end
 
   def storable_location?
-     request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-   end
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
 
-   def store_user_location!
-     store_location_for(:user, request.fullpath)
-   end
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 
   def after_sign_in_path_for(resource_or_scope)
     create_instances_from_session
