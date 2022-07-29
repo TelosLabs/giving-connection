104166e97893f3aba457f3a1186b836a3670ac43
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-29 17:08:52 -0500

    decorator updated

--- app/decorators/delivering_message.rb
+++ app/decorators/delivering_message.rb
@@ -2,19 +2,9 @@
 
 class DeliveringMessage < SimpleDelegator
   def save
-    update_message_subject(__getobj__)
     if __getobj__.save
       MessageMailer.default_response(__getobj__).deliver_later
       MessageMailer.admins_notification(__getobj__).deliver_later
     end
-  end
-
-  private
-
-  def update_message_subject(message)
-    message.subject = 'I want to publish a nonprofit on Giving Connection' if message.subject == '1'
-    message.subject = 'I want to claim ownership of a nonprofit page' if message.subject == '2'
-    message.subject = 'Other' if message.subject == '3'
-    message
   end
 end
