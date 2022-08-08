783511d074c2b2797a2b6c0e8a9fe4caedb01362
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-08 18:00:47 -0500

    Sequence added for emails

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    email { 'user@example.com' }
+    sequence(:name) { |n| "user#{n}@example.com" }
     password { 'testing' }
     password_confirmation { 'testing' }
   end
