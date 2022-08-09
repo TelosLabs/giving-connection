5d1a3a6de89c511cf9e1531d9415238138e8b8e8
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-08 19:10:31 -0500

    Office hours added

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -28,7 +28,7 @@ class Organization < ApplicationRecord
   include Organizations::Constants
   validates_with OrganizationValidator
 
-  scope :active, -> { where(active: true) } 
+  scope :active, -> { where(active: true) }
 
   has_many :tags, dependent: :destroy
   has_many :organization_causes, dependent: :destroy
--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
     address { 'MyString' }
     latitude { 1.5 }
     longitude { 1.5 }
-    organization { association(:organization) }
   end
 end
