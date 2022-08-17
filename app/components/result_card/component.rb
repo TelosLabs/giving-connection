baed25969dcff3551a2000ca206d69139ef0012f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-17 15:42:38 -0500

    verified only working for shor names

--- app/components/result_card/component.rb
+++ app/components/result_card/component.rb
@@ -3,7 +3,8 @@ class ResultCard::Component < ViewComponent::Base
   include ApplicationHelper
 
   def initialize(title:, address:, image_url:, website:, description:, id:, current_user:, phone_number:, verified: )
-    @title = title
+    # <span><%= image_tag 'verified_nonprofit_check.jpg', class: "w-4 h-4 ml-2" %></span>
+    @title = verified ? "#{title}✅" : title
     @address = address
     @image_url = image_url
     @website = website
@@ -30,5 +31,10 @@ class ResultCard::Component < ViewComponent::Base
 
   def formated_description
     @description.length <= 280 ? @description : "#{@description[0..280]} (...)"
+  end
+
+  def logo
+    data = open("http://www.gravatar.com/avatar/c2713a4959692f16d27d2553fb06cc4b.png?r=x&s=170")
+    send_data data, type: image.content_type, disposition: 'inline'
   end
 end
