eb177be6e5fd3c895ad226a48451c5fcc2738037
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-22 14:31:11 -0500

    File added

--- spec/factories/organization_causes.rb
+++ spec/factories/organization_causes.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
-  factory :organization_causes do
-    factory :organization_causes_skips_validate do
-      to_create {|instance| instance.save(validate: false) }
-    end
+  factory :organization_cause do
+    cause { create(:cause) }
   end
 end
