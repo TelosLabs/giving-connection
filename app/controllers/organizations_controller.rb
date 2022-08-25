3b43c98f33b9c405ebc28117bee8b441c72d63c0
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-25 12:19:50 -0500

    Fields appearing in each location

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -117,10 +117,10 @@ class OrganizationsController < ApplicationController
     params.require(:organization)
           .permit(:name, :second_name, :ein_number, :irs_ntee_code, :website, :scope_of_work,
                   :mission_statement_en, :mission_statement_es, :vision_statement_en, :logo,
-                  :vision_statement_es, :tagline_en, :tagline_es, :email, :phone_number, :active, :verified, :youtube_video_link,
+                  :vision_statement_es, :tagline_en, :tagline_es, :email, :phone_number, :active, :verified,
                   social_media_attributes: %i[facebook instagram twitter linkedin youtube blog id],
                   tags_attributes: [],
-                  locations_attributes: [:id, :name, :address, :latitude, :longitude, :website, :po_box,
+                  locations_attributes: [:id, :name, :address, :latitude, :longitude, :website, :po_box, :youtube_video_link,
                                          :main, :physical, :offer_services, :appointment_only, :email, :_destroy,
                                          { phone_number_attributes: [:number],
                                            office_hours_attributes: %i[id day open_time close_time closed]
