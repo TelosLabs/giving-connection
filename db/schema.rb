bb2307924492126bd3168daa2097dd2d6694d786
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-25 11:18:45 -0500

    Migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_184357) do
+ActiveRecord::Schema.define(version: 2022_08_25_161720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -177,6 +177,7 @@ ActiveRecord::Schema.define(version: 2022_08_02_184357) do
     t.string "email"
     t.boolean "po_box", default: false
     t.string "suite"
+    t.string "youtube_video"
     t.index ["lonlat"], name: "index_locations_on_lonlat", using: :gist
     t.index ["organization_id"], name: "index_locations_on_organization_id"
   end
