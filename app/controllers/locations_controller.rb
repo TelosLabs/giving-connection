3ae5d3eb43a7f2026244bbae8bcd1576ab2974a9
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-25 13:51:01 -0500

    Video working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,6 +6,7 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
     authorize @location
+    @youtube_video_id = you_toube_id(@location.youtube_video_link)
   end
 
   def new
@@ -21,6 +22,12 @@ class LocationsController < ApplicationController
   private
 
   def create_params
-    params.require(:location).permit(:address, :longitude, :latitude)
+    params.require(:location).permit(:address, :longitude, :latitude, :youtube_video_link)
+  end
+
+  def you_toube_id(youtube_video_link)
+    regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    match = regex.match(youtube_video_link)
+    match && !match[1].blank? ? match[1] : nil
   end
 end
