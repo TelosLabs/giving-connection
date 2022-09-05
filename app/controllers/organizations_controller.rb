ceafdb34b7f6f77a021f04572f123d7b3622054d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-05 08:35:35 -0500

    migration added

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -120,7 +120,7 @@ class OrganizationsController < ApplicationController
                   social_media_attributes: %i[facebook instagram twitter linkedin youtube blog id],
                   tags_attributes: [],
                   locations_attributes: [:id, :name, :address, :latitude, :longitude, :website, :po_box, :youtube_video_link,
-                                         :main, :physical, :offer_services, :appointment_only, :email, :_destroy,
+                                         :main, :physical, :offer_services, :appointment_only, :email, :_destroy, :images,
                                          { phone_number_attributes: [:number],
                                            office_hours_attributes: %i[id day open_time close_time closed]
                                          }],
