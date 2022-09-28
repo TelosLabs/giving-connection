e832442133484a05d075e95dd0703d96bc3bd47f
Josue Granados <josue@teloslabs.co>
2022-09-28 17:37:38 -0500

    pill and button compoonents refactored. Counter button positioned on top of tabs

--- app/components/search_pills/pill/component.rb
+++ app/components/search_pills/pill/component.rb
@@ -1,11 +1,15 @@
 require "securerandom"
 
 class SearchPills::Pill::Component < ViewComponent::Base
-  def initialize(form:, record_attr:, checked:, value:)
+  def initialize(form:, record_attr:, options: {}, value:)
     @form = form
     @record_attr = record_attr
-    @checked = checked
+    @options = options.merge(
+      {
+        class: "hidden pill",
+        id: SecureRandom.alphanumeric
+      }
+    )
     @value = value
-    @checkbox_id = SecureRandom.alphanumeric
   end
 end
