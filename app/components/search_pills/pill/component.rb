59d37ae77df00908fd87439ff3129f7ba7bb06c9
Josue Granados <josue@teloslabs.co>
2022-10-04 11:43:37 -0500

    Pill and radio button components refactored. Search pills general code abstracted from the search show view.

--- app/components/search_pills/pill/component.rb
+++ app/components/search_pills/pill/component.rb
@@ -1,15 +1,15 @@
 require "securerandom"
 
 class SearchPills::Pill::Component < ViewComponent::Base
-  def initialize(form:, record_attr:, options: {}, value:)
-    @form = form
-    @record_attr = record_attr
+  def initialize(name:, value: , checked:,  options: {})
+    @name = name
+    @value = value
+    @checked = checked
     @options = options.merge(
       {
         class: "hidden pill",
         id: SecureRandom.alphanumeric
       }
     )
-    @value = value
   end
 end
