df907cc12d74fb2d1da1e25371f54c93dbec9e7d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-10-26 14:35:07 -0500

    Helper Refactored

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -13,13 +13,13 @@ module SearchesHelper
     list.flatten.compact
   end
 
-  def selected_services_pills(object, top_10_services)
-    list = []
-    top_10_services = top_10_services.map(&:name)
-    object.services&.map(&:last)&.flatten&.each do |service|
-      list << service if top_10_services.include?(service)
-    end
-    list.flatten.compact.join(", ")
+  def selected_pills(tab_pills, type)
+    return '' unless params[:search] && params[:search][type].present?
+
+    tab_pills = tab_pills.map(&:name)
+    search = params[:search][type.to_sym]
+    search = search.values.flatten if type.include?('services') || type.include?('beneficiary_groups')
+    tab_pills & search
   end
 
   def list_of_services(object, top_10_services)
@@ -29,29 +29,11 @@ module SearchesHelper
     list.flatten.compact - top_10_services
   end
 
-  def selected_causes_pills(object, top_10_causes)
-    list = []
-    top_10_causes = top_10_causes.map(&:name)
-    object.causes&.flatten&.each do |cause|
-      list << cause if top_10_causes.include?(cause)
-    end
-    list.flatten.compact.join(", ")
-  end
-
   def list_of_causes(object, top_10_causes)
     list = []
     top_10_causes = top_10_causes.map(&:name)
     list << object.causes&.flatten
     list.flatten.compact - top_10_causes
-  end
-
-  def selected_beneficiary_groups_pills(object, top_10_beneficiary_groups)
-    list = []
-    top_10_beneficiary_groups = top_10_beneficiary_groups.map(&:name)
-    object.beneficiary_groups&.map(&:last)&.flatten&.each do |beneficiary_group|
-      list << beneficiary_group if top_10_beneficiary_groups.include?(beneficiary_group)
-    end
-    list.flatten.compact.join(", ")
   end
 
   def list_of_beneficiary_groups(object, top_10_beneficiary_groups)
