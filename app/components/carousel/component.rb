3365c4711dbaf91b0154065bb0eaa35bde3a419b
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-03 11:45:41 -0600

    Carousel class refactored

--- app/components/carousel/component.rb
+++ app/components/carousel/component.rb
@@ -2,6 +2,6 @@
 class Carousel::Component < ViewComponent::Base
   def initialize(location:, design_class: nil)
     @location = location
-    @class = design_class
+    @class = design_class || 'relative h-72 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg'
   end
 end
