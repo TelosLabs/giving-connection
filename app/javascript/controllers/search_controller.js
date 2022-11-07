8066a0b6eeaacc9b65114dc26cd74682206acfac
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-07 12:30:37 -0600

    counter working

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
       "pillsCounterWrapper",
       "filtersIcon",
       "clearAllButton",
-      "advancedSelectedFilters"
+      "advancedFiltersCheckboxes"
     ]
   }
 
@@ -41,18 +41,13 @@ export default class extends Controller {
 
   updatePillsCounter() {
     // selects all checked inputs that are not checkboxAll
-    this.totalChecked = this.pillsTarget.querySelectorAll("input:checked:not([data-checkbox-select-all-target=checkboxAll])").length
-    // this.customInputTargets.forEach(input => {
-    //   this.totalChecked += parseInt(input.dataset.searchAdvancedValue)
-    // })
-
-    console.log(this.advancedSelectedFiltersTarget);
-    this.pillsCounterTarget.textContent = this.totalChecked
+    this.totalChecked = document.querySelectorAll("input:checked").length;
+    this.pillsCounterTarget.textContent = this.totalChecked - 1
     this.formTarget.requestSubmit()
   }
 
   pillsCounterDisplay() {
-    if (this.totalChecked > 0) {
+    if (this.totalChecked - 1 > 0) {
       this.pillsCounterWrapperTarget.classList.remove("hidden")
       this.filtersIconTarget.classList.add("hidden")
     }
