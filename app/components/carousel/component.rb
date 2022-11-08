0f6d791cff240c75ada9ef0e7d11da03beccbeea
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-08 14:29:47 -0600

    Carousel class options refactored

--- app/components/carousel/component.rb
+++ app/components/carousel/component.rb
@@ -1,7 +1,7 @@
 
 class Carousel::Component < ViewComponent::Base
-  def initialize(location:, options: {})
+  def initialize(location:, options: 'relative h-72 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg')
     @location = location
-    @options = options || 'relative h-72 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg'
+    @options = options
   end
 end
