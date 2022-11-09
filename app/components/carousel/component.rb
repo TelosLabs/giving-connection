2405799c82239062855b7de1b258a325bf90ddd6
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-09 10:24:47 -0600

    Partial removed

--- app/components/carousel/component.rb
+++ app/components/carousel/component.rb
@@ -1,6 +1,6 @@
 
 class Carousel::Component < ViewComponent::Base
-  def initialize(location:, options: 'relative h-72 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg')
+  def initialize(location:, options: 'relative h-40 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg')
     @location = location
     @options = options
   end
