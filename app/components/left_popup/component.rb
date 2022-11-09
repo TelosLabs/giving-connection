1f650b8189fbda7d7615b08d0a1624dd3f2aa8b6
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-09 11:15:00 -0600

    Contact Info Moved to Component

--- app/components/left_popup/component.rb
+++ app/components/left_popup/component.rb
@@ -5,10 +5,5 @@ module LeftPopup
       @options = options
       @current_user = current_user
     end
-
-    def display_day_working_hours(office_hour)
-      return "Closed" if office_hour.closed?
-      "#{office_hour.open_time.strftime("%l %p")}-#{office_hour.close_time.strftime("%l %p")} (CST)"
-    end
   end
 end
