f9e6107e92c83b499ee482b54b01d81809d70d00
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-10 08:12:55 -0600

    MapLeftPopup height

--- tailwind.config.js
+++ tailwind.config.js
@@ -107,6 +107,7 @@ module.exports = {
         '400px': '400px',
         '85vh': '85vh',
         '500px': '500px',
+        '722px': '722px',
       },
       borderRadius: {
         '6px': '6px',
