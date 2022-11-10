1e669d5817ab6e3fa696f0ef5d6bd930ea367bc1
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-10 09:35:13 -0600

    Using decorator

--- app/components/working_hours/component.rb
+++ app/components/working_hours/component.rb
@@ -8,12 +8,6 @@ module WorkingHours
     def initialize(result:)
       @result = result
     end
-
-    def display_day_working_hours(office_hour)
-      return 'Closed' if office_hour.closed?
-
-      "#{office_hour.open_time.strftime('%l %p')}-#{office_hour.close_time.strftime('%l %p')} (CST)"
-    end
   end
 end
 
