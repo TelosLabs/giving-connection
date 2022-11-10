2f9c9db4ad753538ddda2eeb7d6fe7c8f691a2db
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-10 08:45:56 -0600

    Adjust Padding in LeftPopup Sections

--- app/components/carousel/component.rb
+++ app/components/carousel/component.rb
@@ -4,7 +4,7 @@
 # rubocop:disable Style/ClassAndModuleChildren
 # rubocop:disable Lint/MissingSuper
 class Carousel::Component < ViewComponent::Base
-  def initialize(location:, options: 'swiper-container h-40 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg my-3 shadow-lg')
+  def initialize(location:, options: 'swiper-container h-40 w-64 md:w-96 md:h-80 overflow-hidden rounded-lg my-3 shadow-2xl')
     @location = location
     @options = options
   end
