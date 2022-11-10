caf7390530bd0482d8460e2ea6549892c8228fe1
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-09 18:20:04 -0600

    Rfeacyor

--- app/components/left_popup/component.rb
+++ app/components/left_popup/component.rb
@@ -11,20 +11,6 @@ module LeftPopup
       @options = options
       @current_user = current_user
     end
-
-    def website_url
-      uri = URI(@website)
-
-      if uri.instance_of?(URI::Generic)
-        split = uri.to_s.split('/')
-        if split.size > 1
-          uri = URI::HTTP.build({host: split.shift, path: '/'+split.join('/')})
-        else
-          uri = URI::HTTP.build({host: split.shift.to_s})
-        end
-      end
-      uri.to_s
-    end
   end
 end
 
