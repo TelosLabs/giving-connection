7866663df1f88482d11bd91134c3acade3ad4ecd
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-18 18:44:49 -0600

    perf<serch_controller.js> remove extra call

--- app/components/search_pills/button/component.rb
+++ app/components/search_pills/button/component.rb
@@ -11,7 +11,7 @@ class SearchPills::Button::Component < ViewComponent::Base
         class: "hidden pill",
         id: SecureRandom.alphanumeric,
         data: {
-          action: "change->search#updatePillsCounter change->search#updateFiltersState change->search#submitForm",
+          action: "change->search#updateFiltersState change->search#submitForm",
         }
       }
     )
