cb9fe475b53893035fd3cf79d2f577e644b7d6d8
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-21 08:02:30 -0600

    refactor<check_box_select_all.js> build options refactored

--- app/components/search_pills/pill/component.rb
+++ app/components/search_pills/pill/component.rb
@@ -2,18 +2,28 @@ require "securerandom"
 
 class SearchPills::Pill::Component < ViewComponent::Base
   def initialize(name:, value: , checked:,  options: {})
+    options = build_options(options) if options[:data].present?
     @name = name
     @value = value
     @checked = checked
-    @options = options.merge(
-      {
-        class: 'hidden pill',
-        id: SecureRandom.alphanumeric,
-        data: {
-          action: "#{options[:data] ? options[:data][:action] : ''} change->places#hidePopup change->search#updateFiltersState change->search#submitForm",
-          checkbox_select_all_target: options[:data] ? options[:data][:checkbox_select_all_target] : ''
-        }
+    @options = {
+      class: 'hidden pill',
+      id: SecureRandom.alphanumeric,
+      data: {
+        multiple: options[:multiple],
+        checkbox_select_all_target: options[:checkbox_select_all_target],
+        action: "#{options[:action]} change->places#hidePopup change->search#updateFiltersState change->search#submitForm"
       }
-    )
+    }
+  end
+
+  private
+
+  def build_options(options)
+    hash = {}
+    hash[:action] = options[:data][:action] || ''
+    hash[:multiple] = options[:data][:multiple] || ''
+    hash[:checkbox_select_all_target] = options[:data][:checkbox_select_all_target] || ''
+    hash
   end
 end
