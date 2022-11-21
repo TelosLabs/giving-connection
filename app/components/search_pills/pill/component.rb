c8a23abca8e004c1fee5f8f60faff4dcc340059d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-11-21 09:07:23 -0600

    refactor<check_box_select_all.js> create keys

--- app/components/search_pills/pill/component.rb
+++ app/components/search_pills/pill/component.rb
@@ -2,28 +2,17 @@ require "securerandom"
 
 class SearchPills::Pill::Component < ViewComponent::Base
   def initialize(name:, value: , checked:,  options: {})
-    options = build_options(options) if options[:data].present?
     @name = name
     @value = value
     @checked = checked
-    @options = {
-      class: 'hidden pill',
-      id: SecureRandom.alphanumeric,
-      data: {
-        multiple: options[:multiple],
-        checkbox_select_all_target: options[:checkbox_select_all_target],
-        action: "#{options[:action]} change->places#hidePopup change->search#updateFiltersState change->search#submitForm"
+    @options = options.merge(
+      {
+        class: 'hidden pill',
+        id: SecureRandom.alphanumeric
       }
-    }
-  end
-
-  private
-
-  def build_options(options)
-    hash = {}
-    hash[:action] = options[:data][:action] || ''
-    hash[:multiple] = options[:data][:multiple] || ''
-    hash[:checkbox_select_all_target] = options[:data][:checkbox_select_all_target] || ''
-    hash
+    )
+    @options[:data] ||= {}
+    @options[:data][:action] ||= ''
+    @options[:data][:action] += ' change->places#hidePopup change->search#updateFiltersState change->search#submitForm'
   end
 end
