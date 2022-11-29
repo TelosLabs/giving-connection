90534afb31d3037c100f47a4749b017bf67c3652
Josue Granados <josue@teloslabs.co>
2022-11-28 18:08:05 -0600

    icons in search result cards

--- app/components/circular_icon/component.rb
+++ app/components/circular_icon/component.rb
@@ -7,7 +7,7 @@ class CircularIcon::Component < ViewComponent::Base
 
   def wrapper_options
     {
-      class: "grid place-items-center rounded-full mb-2"
+      class: "grid place-items-center rounded-full"
     }.merge(@wrapper_options) { |duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
   end
 end
