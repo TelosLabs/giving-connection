ce8e39d85029a0a3eeae08981389c0d625a250b5
Daniel Enriquez <daniel.enm17@gmail.com>
2022-12-02 13:44:56 -0600

    fix<gh workflow> gemfile updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.prepend_before(:each, type: :system) do
+    driven_by Capybara.javascript_driver
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
