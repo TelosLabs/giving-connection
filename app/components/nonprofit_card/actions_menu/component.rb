3701387d45c16639b882ff2e5784b529b47bfd26
Josue Granados <josue@teloslabs.co>
2022-12-02 14:43:28 -0600

    More roubust website action

--- app/components/nonprofit_card/actions_menu/component.rb
+++ app/components/nonprofit_card/actions_menu/component.rb
@@ -17,12 +17,13 @@ class NonprofitCard::ActionsMenu::Component < ViewComponent::Base
   end
 
   def website_action_params
+    existing_website_url = @location.decorate.website || @location.organization&.decorate&.website
     {
-      href: @location.website || "javascript:void(0);",
-      target: ("_blank" if @location.website),
-      button_styles: @location.website ? button_styles : "relative grid place-items-center pb-4 text-gray-4 cursor-auto",
-      icon_wrapper_styles: @location.website ? icon_wrapper_styles : "w-10 h-10 border border-gray-4",
-      icon_styles: ("disabled-icon" unless @location.website)
+      href: existing_website_url || "javascript:void(0);",
+      target: ("_blank" if existing_website_url),
+      button_styles: existing_website_url ? button_styles : "relative grid place-items-center pb-4 text-gray-4 cursor-auto",
+      icon_wrapper_styles: existing_website_url ? icon_wrapper_styles : "w-10 h-10 border border-gray-4",
+      icon_styles: ("disabled-icon" unless existing_website_url)
     }
   end
 end
