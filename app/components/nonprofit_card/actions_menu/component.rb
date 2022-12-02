8de6f15f2816114ce1e9a4cb04c375e96b65d7eb
Josue Granados <josue@teloslabs.co>
2022-12-02 14:18:57 -0600

    disabled styles for website action if a non profit doesn't have the former

--- app/components/nonprofit_card/actions_menu/component.rb
+++ app/components/nonprofit_card/actions_menu/component.rb
@@ -15,4 +15,14 @@ class NonprofitCard::ActionsMenu::Component < ViewComponent::Base
   def action_copy_styles
     "absolute bottom-0 right-1/2 w-max transform translate-x-1/2"
   end
+
+  def website_action_params
+    {
+      href: @location.website || "javascript:void(0);",
+      target: ("_blank" if @location.website),
+      button_styles: @location.website ? button_styles : "relative grid place-items-center pb-4 text-gray-4 cursor-auto",
+      icon_wrapper_styles: @location.website ? icon_wrapper_styles : "w-10 h-10 border border-gray-4",
+      icon_styles: ("disabled-icon" unless @location.website)
+    }
+  end
 end
