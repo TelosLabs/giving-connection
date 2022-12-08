13f0f2a4b63cee9ef73157fccf9ceb4931d9d316
Alan Soto <soto.alan@hotmail.com>
2022-12-07 18:19:35 -0600

    Unify Tooltip and Causeslist components

--- app/components/causes_list/component.rb
+++ app/components/causes_list/component.rb
@@ -1,27 +1,44 @@
 class CausesList::Component < ViewComponent::Base
-  def initialize(causes:, navigational: false, list_options: {}, cause_options: {}, icon_wrapper_options: {})
+  def initialize(causes:, tooltip: false, list_options: {}, cause_options: {}, icon_wrapper_options: {}, tooltip_options: {}, icon_svg_options: {})
     @causes = causes
-    # true if we want to use links
-    @navigational = navigational
     @list_options = list_options
     @cause_options = cause_options
     @icon_wrapper_options = icon_wrapper_options
+    @icon_svg_options = icon_svg_options
+    @tooltip = tooltip
+    @tooltip_options = tooltip_options
+    @tooltip_id = SecureRandom.hex
   end
 
-  def cause_options(cause)
+  def path(cause)
     {
-      href: @navigational ? discover_show_path(cause) : nil,
-      class: "grid place-items-center text-center"
-    }.merge(@cause_options) { |duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
+      href: discover_show_path(cause)
+    }
+  end
+
+  def cause_options
+    {
+      class: @tooltip ? "relative" : "grid place-items-center text-center"
+    }.merge(@cause_options) { |_duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
   end
 
   def icon_wrapper_options
     {
       class: "grid place-items-center rounded-full mb-2 bg-blue-pale"
-    }.merge(@icon_wrapper_options) { |duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
+    }.merge(@icon_wrapper_options) { |_duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
   end
 
-  def tag
-    @navigational ? :a : :span
+  def icon_svg_options
+    {
+      class: ""
+    }.merge(@icon_svg_options) { |_duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
+  end
+
+  def tooltip_options
+    {
+      class: @tooltip ? "absolute hidden" : "",
+      role: @tooltip ? "tooltip" : "",
+      id: @tooltip ? @tooltip_id : ""
+    }.merge(@tooltip_options) { |_duplicate_key, existing_value, new_value| "#{existing_value} #{new_value}" }
   end
 end
