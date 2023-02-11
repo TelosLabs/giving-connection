2720e351cb3739e24649d0e0e13fe9e2db7412d5
Josue Granados <contact@josuegranados.com>
2023-02-10 19:30:27 -0600

    Checkbox state management in hours modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,35 +1,39 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
-  static targets = ['container']
+  static targets = ["container"];
   static values = {
-    backdropColor: { type: String, default: 'rgba(0, 0, 0, 0.8)' },
-    restoreScroll: { type: Boolean, default: true }
-  }
+    backdropColor: { type: String, default: "rgba(0, 0, 0, 0.8)" },
+    restoreScroll: { type: Boolean, default: true },
+  };
 
   connect() {
     // The class we should toggle on the container
-    this.toggleClass = this.data.get('class') || 'hidden';
+    this.toggleClass = this.data.get("class") || "hidden";
 
     // The ID of the background to hide/remove
-    this.backgroundId = this.data.get('backgroundId') || 'modal-background';
+    this.backgroundId = this.data.get("backgroundId") || "modal-background";
 
     // The HTML for the background element
-    this.backgroundHtml = this.data.get('backgroundHtml') || this._backgroundHTML();
+    this.backgroundHtml =
+      this.data.get("backgroundHtml") || this._backgroundHTML();
 
     // Let the user close the modal by clicking on the background
-    this.allowBackgroundClose = (this.data.get('allowBackgroundClose') || 'true') === 'true';
+    this.allowBackgroundClose =
+      (this.data.get("allowBackgroundClose") || "true") === "true";
 
     // Prevent the default action of the clicked element (following a link for example) when opening the modal
-    this.preventDefaultActionOpening = (this.data.get('preventDefaultActionOpening') || 'true') === 'true';
+    this.preventDefaultActionOpening =
+      (this.data.get("preventDefaultActionOpening") || "true") === "true";
 
     // Prevent the default action of the clicked element (following a link for example) when closing the modal
-    this.preventDefaultActionClosing = (this.data.get('preventDefaultActionClosing') || 'true') === 'true';
+    this.preventDefaultActionClosing =
+      (this.data.get("preventDefaultActionClosing") || "true") === "true";
 
-    this.appliedIcon = document.querySelector("#appliedIcon")
-    this.modalCheckboxes = Array.from(document.querySelectorAll("input.modal-checkbox"))
-    this.displayAppliedIcon()
-
-
+    this.appliedIcon = document.querySelector("#appliedIcon");
+    this.modalCheckboxes = Array.from(
+      document.querySelectorAll("input.modal-checkbox")
+    );
+    this.displayAppliedIcon();
   }
 
   disconnect() {
@@ -38,25 +42,25 @@ export default class extends Controller {
 
   open(e) {
     // disable button until modal is closed
-      if (this.preventDefaultActionOpening) {
-        e.preventDefault();
-      }
+    if (this.preventDefaultActionOpening) {
+      e.preventDefault();
+    }
 
-      if (e.target.blur) {
-        e.target.blur();
-      }
+    if (e.target.blur) {
+      e.target.blur();
+    }
 
-      // Lock the scroll and save current scroll position
-      this.lockScroll();
+    // Lock the scroll and save current scroll position
+    this.lockScroll();
 
-      // Unhide the modal
-      this.containerTarget.classList.remove(this.toggleClass);
+    // Unhide the modal
+    this.containerTarget.classList.remove(this.toggleClass);
 
-      // Insert the background
-      if (!this.data.get("disable-backdrop")) {
-        document.body.insertAdjacentHTML('beforeend', this.backgroundHtml);
-        this.background = document.querySelector(`#${this.backgroundId}`);
-      }
+    // Insert the background
+    if (!this.data.get("disable-backdrop")) {
+      document.body.insertAdjacentHTML("beforeend", this.backgroundHtml);
+      this.background = document.querySelector(`#${this.backgroundId}`);
+    }
   }
 
   close(event) {
@@ -70,17 +74,19 @@ export default class extends Controller {
     this.containerTarget.classList.add(this.toggleClass);
 
     // Remove the background
-    if (this.background) { this.background.remove() }
+    if (this.background) {
+      this.background.remove();
+    }
   }
 
   clearUnappliedFilters() {
     // gets the query string of the url
-    const queryString = window.location.href.split('?')[1];
+    const queryString = window.location.href.split("?")[1];
     // produces an array of values of the key/value pairs from the query string
     const values = [...new URLSearchParams(queryString).values()];
 
     // validates if all checked filters are applied (present in the query string of the url)
-    this.containerTarget.querySelectorAll("input:checked").forEach(filter => {
+    this.containerTarget.querySelectorAll("input:checked").forEach((filter) => {
       if (!values.includes(filter.value)) {
         // fires data-action that unchecks and removes displayed badges
         filter.click();
@@ -91,12 +97,15 @@ export default class extends Controller {
   closeBackground(e) {
     if (this.allowBackgroundClose && e.target === this.containerTarget) {
       this.close(e);
-      this.clearUnappliedFilters()
+      this.clearUnappliedFilters();
     }
   }
 
   closeWithKeyboard(e) {
-    if (e.keyCode === 27 && !this.containerTarget.classList.contains(this.toggleClass)) {
+    if (
+      e.keyCode === 27 &&
+      !this.containerTarget.classList.contains(this.toggleClass)
+    ) {
       this.close(e);
     }
   }
@@ -108,11 +117,12 @@ export default class extends Controller {
   lockScroll() {
     // Add right padding to the body so the page doesn't shift
     // when we disable scrolling
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
     document.body.style.paddingRight = `${scrollbarWidth}px`;
 
     // Add classes to body to fix its position
-    document.body.classList.add('fixed', 'inset-x-0', 'overflow-hidden');
+    document.body.classList.add("fixed", "inset-x-0", "overflow-hidden");
 
     if (this.restoreScrollValue) {
       // Save the scroll position
@@ -128,7 +138,7 @@ export default class extends Controller {
     document.body.style.paddingRight = null;
 
     // Remove classes from body to unfix position
-    document.body.classList.remove('fixed', 'inset-x-0', 'overflow-hidden');
+    document.body.classList.remove("fixed", "inset-x-0", "overflow-hidden");
 
     // Restore the scroll position of the body before it got locked
     if (this.restoreScrollValue) {
@@ -150,13 +160,35 @@ export default class extends Controller {
   }
 
   displayAppliedIcon() {
-    if (this.modalCheckboxes.some(check => check.checked === true)) {
-      this.appliedIcon.classList.remove("hidden")
-      this.appliedIcon.classList.add("inline-block")
+    if (this.modalCheckboxes.some((check) => check.checked === true)) {
+      this.appliedIcon.classList.remove("hidden");
+      this.appliedIcon.classList.add("inline-block");
+    } else {
+      this.appliedIcon.classList.add("hidden");
+      this.appliedIcon.classList.remove("inline-block");
     }
-    else {
-      this.appliedIcon.classList.add("hidden")
-      this.appliedIcon.classList.remove("inline-block")
-    }
+  }
+
+  /* For checkboxes in modal */
+
+  readCheckboxesState() {
+    const checkboxes = [
+      ...this.containerTarget.querySelectorAll("[type='checkbox']"),
+    ];
+
+    this.checkboxesOriginalState = checkboxes.map(
+      (checkbox) => checkbox.checked
+    );
+  }
+
+  restoreCheckboxesState() {
+    const checkboxes = [
+      ...this.containerTarget.querySelectorAll("[type='checkbox']"),
+    ];
+
+    checkboxes.forEach((checkbox) => {
+      const originalState = this.checkboxesOriginalState.shift();
+      if (checkbox.checked !== originalState) checkbox.checked = originalState;
+    });
   }
 }
