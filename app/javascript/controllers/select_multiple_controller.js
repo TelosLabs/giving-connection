a1855c6e76e9f7105bda3ca9800805337feb3a09
Alan Soto <soto.alan@hotmail.com>
2023-03-15 15:59:18 -0600

    Allow searching

--- app/javascript/controllers/select_multiple_controller.js
+++ app/javascript/controllers/select_multiple_controller.js
@@ -86,7 +86,7 @@ export default class extends Controller {
     const query = this.inputTarget.value
     const regex = new RegExp('.*' + query.toLowerCase() + '.*', 'gmi')
     this.checkboxTargets.forEach(checkbox => {
-      if (checkbox.value.search(regex) >= 0) {
+      if (checkbox.dataset.value.search(regex) >= 0) {
         checkbox.parentElement.classList.remove('hidden')
       } else {
         checkbox.parentElement.classList.add('hidden')
