463c13189b270ad3faafc06d71fa455a95085f70
Josue Granados <contact@josuegranados.com>
2023-04-04 14:34:20 -0600

    Instance variable for popup partial renamed.

--- app/controllers/map_popups_controller.rb
+++ app/controllers/map_popups_controller.rb
@@ -1,9 +1,8 @@
 class MapPopupsController < ApplicationController
-  # TODO: Authorize action
   skip_after_action :verify_authorized
   skip_before_action :authenticate_user!
 
   def new
-    @org = Location.find(params[:location_id])
+    @location = Location.find(params[:location_id])
   end
 end
