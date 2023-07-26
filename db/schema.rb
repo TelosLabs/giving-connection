87067bd333d39a76bb051f50c5ad6ddc02255122
Alan Soto <soto.alan@hotmail.com>
2023-07-26 09:59:19 -0600

    Add public_address attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_191909) do
+ActiveRecord::Schema.define(version: 2023_07_26_001911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -178,6 +178,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_191909) do
     t.boolean "po_box", default: false
     t.string "suite"
     t.string "youtube_video_link"
+    t.boolean "public_address", default: true
     t.index ["lonlat"], name: "index_locations_on_lonlat", using: :gist
     t.index ["organization_id"], name: "index_locations_on_organization_id"
   end
