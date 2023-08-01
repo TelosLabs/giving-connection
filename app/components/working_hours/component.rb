1ba34d5d99df29043da0bc70fbd9b4e3d2e22977
Alicia <aliciapazrojas@gmail.com>
2023-08-01 17:58:05 -0600

    Make phone number clickable and be able to call

--- app/components/working_hours/component.rb
+++ app/components/working_hours/component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 module WorkingHours
   class WorkingHours::Component < ApplicationViewComponent
-    def initialize(result:)
+    def initialize(result:, device:)
       @result = result
+      @device = device
     end
   end
 end
