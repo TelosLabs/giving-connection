225e44339769715e1b06d7bcb1c6e2e370d00335
Alicia <aliciapazrojas@gmail.com>
2023-08-01 18:15:33 -0600

    Send device variable to view components

--- app/components/map_left_popup/component.rb
+++ app/components/map_left_popup/component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 module MapLeftPopup
   class Component < ApplicationViewComponent
-    def initialize(result:, current_user: false)
+    def initialize(result:, current_user: false, device:)
       @result = result
       @current_user = current_user
+      @device = device
     end
   end
 end
