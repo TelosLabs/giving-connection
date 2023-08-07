a84b85df06c078c9d10f1ef9e70d41b5685706af
Alicia <aliciapazrojas@gmail.com>
2023-08-07 08:38:06 -0600

    Set letter opener as delivery method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
-  config.action_mailer.delivery_method     = :smtp
+  config.action_mailer.delivery_method     = :letter_opener
 
   config.action_mailer.default charset: 'utf-8'
 
