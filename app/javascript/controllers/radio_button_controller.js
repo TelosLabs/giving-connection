9b9ea915d83e429dadeeb148f6e3e940f8ec52ee
Alicia <aliciapazrojas@gmail.com>
2023-08-10 21:22:11 -0600

    Document use of name param in stimulus controller

--- app/javascript/controllers/radio_button_controller.js
+++ app/javascript/controllers/radio_button_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   clearSelection(event) {
+    // use the name param to specify the button group from which you want to clear selection
     const checkedElement = document.querySelector(`input[name="${event.params.name}"]:checked`)
     if (checkedElement) {
       checkedElement.checked = false
