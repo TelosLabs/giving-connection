006d04bf98244c71b7b8890743b04af359dc0092
Alan Soto <soto.alan@hotmail.com>
2023-10-06 16:21:46 -0600

    Decorate urls

--- app/decorators/organization_decorator.rb
+++ app/decorators/organization_decorator.rb
@@ -1,4 +1,11 @@
 class OrganizationDecorator < ApplicationDecorator
   delegate_all
 
+  def donation_link
+    object.decorate.url(object.donation_link)
+  end
+
+  def volunteer_link
+    object.decorate.url(object.volunteer_link)
+  end
 end
