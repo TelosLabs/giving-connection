7bd701e247370b2234111ee3dcd939f12cfb05b6
Alicia <aliciapazrojas@gmail.com>
2024-04-05 17:13:11 -0600

    Filter discover results by location

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -11,7 +11,15 @@ class CausesController < ApplicationController
   def show
     @cause = Cause.find_by(name: params[:name])
     authorize @cause
-    filtered_locations = Location.locations_with_(@cause)
-    @locations_by_services = Location.sort_by_more_services(filtered_locations)
+    @search = Search.new(
+      city: @current_location[:city],
+      state: @current_location[:state],
+      lat: @current_location[:latitude],
+      lon: @current_location[:longitude],
+      causes: [@cause.name]
+    )
+    @search.save
+    Rails.logger.error @search.errors.full_messages if @search.results.any?
+    @locations_by_services = Location.sort_by_more_services(@search.results)
   end
 end
