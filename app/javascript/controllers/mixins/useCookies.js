123b6e64dea03ed6d823e16060fe56ed8f975331
Alicia <aliciapazrojas@gmail.com>
2024-04-17 16:27:25 -0600

    Refactor get cookie method

--- app/javascript/controllers/mixins/useCookies.js
+++ app/javascript/controllers/mixins/useCookies.js
@@ -1,7 +1,8 @@
 export const useCookies = controller => {
   Object.assign(controller, {
     getCookie(key) {
-      return document.cookie.split('; ').find(row => row.startsWith(`${key}=`))?.split('=')[1] || null;
+      const cookieValue = document.cookie.split('; ').find(row => row.startsWith(`${key}=`))?.split('=')[1];
+      return cookieValue || null;
     },
 
     setCookie(key, value) {
