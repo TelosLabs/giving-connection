7a6651c7ba20c0e9ea290eb9eddb5f1a990c87e3
Alicia <aliciapazrojas@gmail.com>
2024-05-23 17:20:35 -0600

    Fix behavior of used EIN number warning message

---
+++ app/javascript/administrate/controllers/uniqueness_warning_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from "stimulus-use";
+
+export default class extends Controller {
+    static targets = ["input"]
+    static values = { url: String }
+
+    connect() {
+        useClickOutside(this)
+        this.confirmedMessage = false
+    }
+
+    checkUniqueness() {
+      const inputValue = this.inputTarget.value;
+      fetch(`${this.urlValue}?ein_number=${inputValue}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.exists && !this.confirmedMessage) {
+            if (!confirm("There is already an organization with that EIN. Are you sure you want to use it?")) {
+              this.inputTarget.value = ''; // Clear the input if not confirmed
+            } else {
+              this.confirmedMessage = true // Prevent the message from showing again
+            }
+          }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+}
