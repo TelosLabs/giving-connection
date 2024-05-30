d14a008d6150ffb58397ba7c454f309f2183a170
Alicia <aliciapazrojas@gmail.com>
2024-05-30 11:07:30 -0600

    Fix office hour time zone method

--- spec/models/office_hour_spec.rb
+++ spec/models/office_hour_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe OfficeHour, type: :model do
     it { is_expected.to validate_presence_of(:open_time) }
     it { is_expected.to validate_presence_of(:close_time) }
   end
+
+  describe "methods" do
+    describe "#time_zone" do
+      it "is expected to return time zone as Time Zone object" do
+        expect(office_hour.time_zone).to be_a(ActiveSupport::TimeZone)
+      end
+
+      it "is expected to have a name that matches the location's time zone " do
+        expect(office_hour.time_zone.name).to eql(location.time_zone)
+      end
+
+      it "is expected to fallback to default time zone if location time zone is not present" do
+        location.time_zone = nil
+        expect(office_hour.time_zone.name).to eql("Eastern Time (US & Canada)")
+      end
+    end
+  end
 end
