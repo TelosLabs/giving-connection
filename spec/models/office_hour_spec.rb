a52d3d4a31c36b7fbfe7aa49611debd0569d8c45
Alicia <aliciapazrojas@gmail.com>
2024-06-06 16:24:25 -0600

    WIP - refactoring time zone convertible

--- spec/models/office_hour_spec.rb
+++ spec/models/office_hour_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe OfficeHour, type: :model do
   let(:location) { create(:location, :with_office_hours) }
   let(:office_hour) { build(:office_hour, location: location) }
 
-  subject { office_hour }
-
   describe "associations" do
     it "is expected to belong to location" do
       expect(office_hour.location).to eql(location)
@@ -13,6 +11,8 @@ RSpec.describe OfficeHour, type: :model do
   end
 
   describe "validations" do
+    subject { office_hour }
+
     it { is_expected.to validate_presence_of(:day) }
     it { is_expected.to validate_inclusion_of(:day).in_range(0..6) }
     it { is_expected.to validate_presence_of(:open_time) }
@@ -33,6 +33,23 @@ RSpec.describe OfficeHour, type: :model do
         location.time_zone = nil
         expect(office_hour.time_zone.name).to eql("Eastern Time (US & Canada)")
       end
+    end
+  end
+
+  xdescribe "callbacks" do
+    let(:location) { create(:location, :with_office_hours, time_zone: "Pacific Time (US & Canada)") }
+    let(:oh) { build(:office_hour, location: location, open_time: "12:00", close_time: "16:00") }
+
+    subject { oh }
+
+    it "converts the time zone from the location to EST before saving" do
+      expect(oh).to receive(:convert_time_to_est)
+      oh.save!
+
+      expect(oh.open_time.zone).to eql("EST")
+      expect(oh.close_time.zone).to eql("EST")
+      expect(oh.open_time.hour).to eql(15)
+      expect(oh.close_time.hour).to eql(19)
     end
   end
 end
