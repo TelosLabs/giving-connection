20dbbac6a3faa1fd17033147a10919c20565a7ad
Alicia <aliciapazrojas@gmail.com>
2024-09-17 20:41:49 -0600

    Test object's base state

--- spec/services/image_heic_to_jpeg_converter_spec.rb
+++ spec/services/image_heic_to_jpeg_converter_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe ImageHeicToJpegConverter, type: :service do
 
   describe "#convert" do
     it "converts heic image to jpeg" do
+      expect(MiniMagick::Image.open(heic_image_path).type).to eq("HEIC")
+
       converted_image = subject.convert
 
       expect(converted_image).to be_a(MiniMagick::Image)
