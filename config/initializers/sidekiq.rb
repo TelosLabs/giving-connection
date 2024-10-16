4d140412cc0b800bc827825e97acd6f171b79ca1
Alicia <aliciapazrojas@gmail.com>
2024-10-16 13:36:36 -0600

    Update SSL settings for heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,9 +7,15 @@ else
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: REDIS_URL}
+  config.redis = {
+    url: REDIS_URL,
+    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: REDIS_URL}
+  config.redis = {
+    url: REDIS_URL,
+    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+  }
 end
