f491e9acf8eecf51bd67b35017390e7ce14592c9
Matthew Vandenberg <mv5903@gmail.com>
2024-10-26 15:52:13 -0400

    Fix macos sed syntax again

--- setup-docker.sh
+++ setup-docker.sh
@@ -5,17 +5,24 @@ sleep 1
 echo "This command will take at least 2 full minutes to complete."
 sleep 1
 
+MAC=false
 # Determine if the platform is macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  SED_OPTION='-i '''
+  MAC=true
+  echo -e "\033[1;33mRunning on macOS.\033[0m"
 else
-  SED_OPTION='-i'
+  echo -e "\033[1;33mRunning on Linux.\033[0m"
 fi
+
 
 # Check and modify .env file
 if ! grep -q "redis://redis:6379" .env; then
   echo -e "\033[1;32mModifying .env file...\033[0m"
-  sed $SED_OPTION 's|redis://localhost:6379|redis://redis:6379|g' .env
+  if $MAC; then
+    sed -i '' 's|redis://localhost:6379|redis://redis:6379|g' .env
+  else
+    sed -i 's|redis://localhost:6379|redis://redis:6379|g' .env
+  fi
 else
   echo -e "\033[1;33m.env file already modified.\033[0m"
 fi
@@ -23,7 +30,11 @@ fi
 # Check and modify Gemfile
 if ! grep -q 'ruby "~> 3.1.0"' Gemfile; then
   echo -e "\033[1;32mModifying Gemfile...\033[0m"
-  sed $SED_OPTION 's|ruby "3.1.0"|ruby "~> 3.1.0"|g' Gemfile
+  if $MAC; then
+    sed -i '' 's|ruby "3.1.0"|ruby "~> 3.1.0"|g' Gemfile
+  else
+    sed -i 's|ruby "3.1.0"|ruby "~> 3.1.0"|g' Gemfile
+  fi
 else
   echo -e "\033[1;33mGemfile already modified.\033[0m"
 fi
@@ -31,24 +42,44 @@ fi
 # Check and modify config/database.yml for host and username
 if grep -q "#username: giving_connection" config/database.yml; then
   echo -e "\033[1;32mModifying config/database.yml file...\033[0m"
-  sed $SED_OPTION 's|#username: giving_connection|username: postgres|g' config/database.yml
+  if $MAC; then
+    sed -i '' 's|#username: giving_connection|username: postgres|g' config/database.yml
+  else 
+    sed -i 's|#username: giving_connection|username: postgres|g' config/database.yml
+  fi
 else
   echo -e "\033[1;33mconfig/database.yml file already modified.\033[0m"
 fi
 
+# Modify specific lines in config/database.yml if not set
 if grep -q "#host: localhost" config/database.yml; then
-  sed $SED_OPTION 's|#host: localhost|host: db|g' config/database.yml
+  if $MAC; then
+    sed -i '' 's|#host: localhost|host: db|g' config/database.yml
+  else
+    sed -i 's|#host: localhost|host: db|g' config/database.yml
+  fi
 fi
 
 if ! sed -n '62p' config/database.yml | grep -q "host: db"; then
-  sed $SED_OPTION '62i\
+  if $MAC; then
+    sed -i '' '62i\
   host: db' config/database.yml
+  else
+    sed -i '62i\
+  host: db' config/database.yml
+  fi
 fi
 
 if ! sed -n '63p' config/database.yml | grep -q "username: postgres"; then
-  sed $SED_OPTION '63i\
+  if $MAC; then
+    sed -i '' '63i\
   username: postgres' config/database.yml
+  else
+    sed -i '63i\
+  username: postgres' config/database.yml
+  fi
 fi
+
 
 
 # Finished editing, now building the image and initializing the container
