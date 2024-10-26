6b80db03a536fc929c6f8c791d10843f609960a0
Matthew Vandenberg <mv5903@gmail.com>
2024-10-26 13:36:34 -0400

    setup: fix db auth bug, only edit config files if not editted already

--- setup-docker.sh
+++ setup-docker.sh
@@ -5,25 +5,48 @@ sleep 1
 echo "This command will take at least 2 full minutes to complete."
 sleep 1
 
-echo -e "\033[1;34mMaking necessary file changes...\033[0m"
-# Make necessary file changes
-sed -i '' 's|redis://localhost:6379|redis://redis:6379|g' .env
-sed -i '' 's|ruby "3.1.0"|ruby "~> 3.1.0"|g' Gemfile
+#echo -e "\033[1;34mMaking necessary file changes...\033[0m"
 
-sed -i '' 's|#username: giving_connection|username: postgres|g' config/database.yml
-sed -i '' 's|#host: localhost|host: db|g' config/database.yml
+# Check and modify .env file
+if ! grep -q "redis://redis:6379" .env; then
+  echo -e "\033[1;32mModifying .env file...\033[0m"
+  sed -i '' 's|redis://localhost:6379|redis://redis:6379|g' .env
+else
+  echo -e "\033[1;33m.env file already modified.\033[0m"
+fi
 
-sed -i '' '62i\
+# Check and modify Gemfile
+if ! grep -q 'ruby "~> 3.1.0"' Gemfile; then
+  echo -e "\033[1;32mModifying Gemfile...\033[0m"
+  sed -i '' 's|ruby "3.1.0"|ruby "~> 3.1.0"|g' Gemfile
+else
+  echo -e "\033[1;33mGemfile already modified.\033[0m"
+fi
+
+# Check and modify config/database.yml for host and username
+if grep -q "#username: giving_connection" config/database.yml; then
+  echo -e "\033[1;32mModifying config/database.yml file...\033[0m"
+  sed -i '' 's|#username: giving_connection|username: postgres|g' config/database.yml
+else
+  echo -e "\033[1;33mconfig/database.yml file already modified.\033[0m"
+fi
+
+if grep -q "#host: localhost" config/database.yml; then
+  sed -i '' 's|#host: localhost|host: db|g' config/database.yml
+fi
+
+if ! sed -n '62p' config/database.yml | grep -q "host: db"; then
+  sed -i '' '62i\
   host: db' config/database.yml
-sed -i '' '63i\
+fi
+
+if ! sed -n '63p' config/database.yml | grep -q "username: postgres"; then
+  sed -i '' '63i\
   username: postgres' config/database.yml
-
-echo "Appending modified files to .gitignore..."
-echo ".env" >> .gitignore
-echo "config/database.yml" >> .gitignore
-echo "Gemfile" >> .gitignore
+fi
 
 
+# Finished editing, now building the image and initializing the container
 echo -e "\033[1;34mPulling the image and initializing the container (this will take some time!)...\033[0m"
 sleep 1
 echo ""
