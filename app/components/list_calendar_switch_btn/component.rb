a69452693cd25555594015557090ec03de09cbe3
Aashita <aashiyaaaa14@gmail.com>
2024-12-05 13:51:00 -0500

    fixed list/cal buttons

--- app/components/list_calendar_switch_btn/component.rb
+++ app/components/list_calendar_switch_btn/component.rb
@@ -20,35 +20,5 @@ class ListCalBtn::Component < ApplicationViewComponent
         }
       end
     end
-  
-    def button_styles
-      @simplified ? "relative" : action_button_styles[:button]
-    end
-  
-    def wrapper_styles
-      action_button_styles[:icon_wrapper] unless @simplified
-    end
-  
-    def copy_styles
-      @simplified ? "sr-only" : action_button_styles[:copy]
-    end
-  
-    def icon_name
-      removable_fav_location? ? "bookmark_colored.svg" : "bookmark_empty.svg"
-    end
-  
-    def removable_fav_location?
-      @user.present? && FavoriteLocation.exists?(user_id: @user&.id, location_id: @location.id)
-    end
-  
-    def favorite_location
-      FavoriteLocation.find_by(user_id: @user.id, location_id: @location.id)
-    end
-  
-    def btn_selector
-      selector = "save-location-#{@location.id}-btn"
-      selector << "__simplified" if @simplified
-      selector
-    end
   end
   
