370c06a2bc893c5deb2ce08c3fad768499f86b70
Alicia <aliciapazrojas@gmail.com>
2025-01-23 12:42:35 -0600

    Increase timeout for system specs

--- spec/system/support/cuprite_setup.rb
+++ spec/system/support/cuprite_setup.rb
@@ -12,7 +12,7 @@ Capybara.register_driver(:better_cuprite) do |app|
     # See additional options for Dockerized environment in the respective section of this article
     browser_options: {},
     # Increase Chrome startup wait time (required for stable CI builds)
-    process_timeout: 10,
+    process_timeout: 15,
     # Enable debugging capabilities
     inspector: true,
     # Allow running Chrome in a headful mode by setting HEADLESS env
