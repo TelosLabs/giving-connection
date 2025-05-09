2d43bd4f96d20f78d3998ef7bfb4c475b8ea8d6b
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-05-09 17:49:11 -0600

    wip office hours parser and rspec test

---
+++ app/services/spreadsheet_import/importer.rb
@@ -0,0 +1,10 @@
+module SpreadhsheetImporter
+  class Importer
+    def initialize(spreadsheet, creator)
+      @spreadsheet = spreadsheet
+      @creator = creator
+      @file_path = File.open(Rails.root.join("db/uploads").to_s)
+      @csv_file_paths = csv_file_paths
+    end
+  end
+end
