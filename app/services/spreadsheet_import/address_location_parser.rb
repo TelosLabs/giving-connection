a1eb0ced86f3558caaf820e3e2aa39c1aef2720d
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-05-15 17:44:10 -0600

    Address location parser

--- app/services/spreadsheet_import/address_location_parser.rb
+++ app/services/spreadsheet_import/address_location_parser.rb
@@ -4,12 +4,21 @@ module SpreadsheetImport
   class AddressLocationParser
 
     def initialize(address)
-      @address = address
+      @address = clean_address(address)
     end
 
     def call
       Geocoder.search(@address).first
     end
-    
+
+    private
+
+    def clean_address(raw_address)
+      address = raw_address.strip
+      address = address.gsub(/[\t\r\n]+/, ' ')
+      address = address.gsub(/\s+/, ' ')
+      address
+    end
+
   end
 end
