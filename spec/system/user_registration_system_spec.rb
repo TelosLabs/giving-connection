813ee6050b3aaeb0a2a3f7751045a01541682a03
Jorge Adrian de la Garza Flores <52385984+jdelagarzaf@users.noreply.github.com>
2025-06-05 09:06:11 -0600

    Update terms (#664)
    * updated terms of use
    
    * updates privacy policy
    
    * Updates last update dates
    
    * Added missing link
    
    * Added checkbox to agree to terms and policy
    
    * Rubocop fix
    
    * Fixed user registration test
    
    * Fix RuboCop offenses

--- spec/system/user_registration_system_spec.rb
+++ spec/system/user_registration_system_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "User Registration", type: :system do
     find(:test_id, "signup_email_input").fill_in with: "jane@example.com"
     find(:test_id, "signup_password_input").fill_in with: "password"
     find(:test_id, "signup_password_confirmation_input").fill_in with: "password"
+    check "user_terms_of_service"
     click_button "signup_submit_btn"
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
