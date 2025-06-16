a3dc2a7a328759cb6ae518d79e56f9657f955dcc
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-06-16 14:33:17 -0600

    fix email on donation link

--- app/decorators/organization_decorator.rb
+++ app/decorators/organization_decorator.rb
@@ -2,11 +2,13 @@ class OrganizationDecorator < ApplicationDecorator
   delegate_all
 
   def donation_link
-    object.decorate.url(object.donation_link)
+    link = object.donation_link.to_s
+    return "mailto:#{link}" if link.match?(/\A[^@\s]+@[^@\s]+\z/)
+    url_or_nil(link)
   end
 
   def volunteer_link
-    object.decorate.url(object.volunteer_link)
+    url_or_nil(object.volunteer_link)
   end
 
   def designation_copies
@@ -28,4 +30,14 @@ class OrganizationDecorator < ApplicationDecorator
       }
     end
   end
+
+  private
+
+  def url_or_nil(link)
+    uri = URI.parse(link)
+    return link if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue URI::InvalidURIError, TypeError
+    nil
+  end
+
 end
