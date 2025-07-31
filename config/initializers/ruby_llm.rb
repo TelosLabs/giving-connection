b963844aaffd439110c8f66d991996de5b46e57e
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-07-31 11:24:24 -0600

    fix gemini provider

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -3,8 +3,6 @@ require "ruby_llm"
 api_key = Rails.application.credentials.dig(:ruby_llm, :gemini)
 raise "Missing api key" unless api_key
 
-client = RubyLLM::GoogleGemini.new(api_key: api_key)
-
 RubyLLM.configure do |config|
-  config.default_client = client
+  config.gemini_api_key = api_key
 end
