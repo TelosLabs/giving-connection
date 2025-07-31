b57576269039f529bb14e22c20a93930a07dd6ef
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-07-31 11:48:07 -0600

    fix typo

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -4,6 +4,6 @@ openai_api_key = Rails.application.credentials.dig(:ruby_llm, :openai)
 gemini_api_key = Rails.application.credentials.dig(:ruby_llm, :gemini)
 
 RubyLLM.configure do |config|
-  configlopenai_api_key = openai_api_key
+  config.openai_api_key = openai_api_key
   config.gemini_api_key = gemini_api_key
 end
