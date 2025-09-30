a2c5efed5c87dcd22d89a2f2d74bd08e748636e4
Alicia <aliciapazrojas@gmail.com>
2025-09-30 13:13:34 -0600

    Remove hardcoded classes

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,9 +1,15 @@
 import { Controller } from '@hotwired/stimulus'
 
+/* global localStorage */
+
 export default class extends Controller {
   static targets = ['tab', 'panel']
+  static values = {
+    activeClasses: { type: String, default: 'border-blue-medium text-blue-medium' },
+    hiddenClass: { type: String, default: 'hidden' }
+  }
 
-  connect() {
+  connect () {
     // Restore active tab from localStorage or default to first tab
     const savedTabName = localStorage.getItem('activeTab')
     if (savedTabName) {
@@ -14,14 +20,14 @@ export default class extends Controller {
     }
     this.updateActiveTab()
   }
-  
-  change(event) {
+
+  change (event) {
     event.preventDefault()
-  
+
     const clickedTab = event.currentTarget
     const tabName = clickedTab.dataset.tabName
     const tabIndex = this.tabTargets.findIndex(tab => tab.dataset.tabName === tabName)
-  
+
     if (tabIndex >= 0) {
       this.index = tabIndex
       localStorage.setItem('activeTab', tabName)
@@ -29,25 +35,26 @@ export default class extends Controller {
       window.dispatchEvent(new CustomEvent('tsc:tab-change'))
     }
   }
-  
-  updateActiveTab() {
+
+  updateActiveTab () {
+    const activeClasses = this.activeClassesValue.split(' ')
+
     this.tabTargets.forEach((tab, i) => {
       if (i === this.index) {
-        tab.classList.add("border-blue-medium", "text-blue-medium")
-        tab.setAttribute("aria-current", "page")
+        tab.classList.add(...activeClasses)
+        tab.setAttribute('aria-current', 'page')
       } else {
-        tab.classList.remove("border-blue-medium", "text-blue-medium")
-        tab.removeAttribute("aria-current")
+        tab.classList.remove(...activeClasses)
+        tab.removeAttribute('aria-current')
       }
     })
-  
+
     this.panelTargets.forEach((panel, i) => {
       if (i === this.index) {
-        panel.classList.remove("hidden")
+        panel.classList.remove(this.hiddenClassValue)
       } else {
-        panel.classList.add("hidden")
+        panel.classList.add(this.hiddenClassValue)
       }
     })
   }
-
 }
