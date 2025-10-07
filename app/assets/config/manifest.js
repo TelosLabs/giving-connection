edbd74803436f83ff8a06b2b8c18d0e7d3fe1d92
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-10-07 14:05:14 -0600

    fix merge conflicts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,6 +3,5 @@
 //= link application.css
 //= link application.tailwind.css
 //= link administrate-field-active_storage/application.css
-//= link administrate-field-trix/application.css
 //= link actiontext.css
 //= link trix.css
