cb682060588e9a5c2f5cf57335f47c30b4b7ea3e
Alicia <aliciapazrojas@gmail.com>
2025-10-16 15:46:17 -0600

    Add geocoder API key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
 Geocoder.configure(
   lookup: :google,
-  api_key: Rails.application.credentials.dig(:ruby_llm, :gemini),
+  api_key: Rails.application.credentials.dig(:google_geocoder_api_key),
   http_headers: {"User-Agent" => "my_rails_app (jorge@teloslabs.co)"},
   timeout: 5,
   units: :km
