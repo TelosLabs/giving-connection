ccb0f95df9b7fc740f0c75a7ec0ab1ca1ebcaa43
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-10-31 17:28:07 -0600

    add character limit and sanitize comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,17 @@ class Comment < ApplicationRecord
   belongs_to :blog
   belongs_to :user
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 1, maximum: 2000, too_long: "is too long (maximum is 2000 characters)" }
+  
+  before_validation :sanitize_body
+
+  private
+
+  def sanitize_body
+    return if body.blank?
+    
+    self.body = ActionController::Base.helpers.sanitize( body, tags: [], attributes: [] ).strip
+
+    self.body = body.squeeze(" ").strip
+  end
 end
