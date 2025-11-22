5b458554c39b48cb232d175638b43dc46f59f6f2
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-11-21 19:40:13 -0600

    Add anonymous user image

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,18 +1,22 @@
 module BlogsHelper
-  def user_avatar(user, size: 'w-8 h-8')
+  def user_avatar(user, size: 'w-8 h-8', fallback_name: nil)
     if user&.avatar&.attached?
       image_tag(user.avatar, alt: "User Avatar", class: "object-cover #{size} rounded-full")
     else
-      name = user.try(:name) || 'Anonymous'
-      initials = name.split.map(&:first).join.upcase.first(2)
-      
-      colors = [
-        'bg-blue-500', 'bg-green-500', 'bg-purple-500', 'bg-pink-500', 
-        'bg-indigo-500', 'bg-red-500', 'bg-yellow-500', 'bg-teal-500'
-      ]
-      color = colors[name.sum % colors.length]
-      
-      content_tag(:div, initials, class: "#{size} rounded-full #{color} text-white flex items-center justify-center font-semibold text-sm")
+      name = user&.name.presence || fallback_name.presence || 'Anonymous'
+      if name == 'Anonymous'
+        image_tag('anonymous-user.jpg', alt: "Anonymous User", class: "object-cover #{size} rounded-full")
+      else
+        initials = name.split.map(&:first).join.upcase.first(2)
+        
+        colors = [
+          'bg-blue-500', 'bg-green-500', 'bg-purple-500', 'bg-pink-500', 
+          'bg-indigo-500', 'bg-red-500', 'bg-yellow-500', 'bg-teal-500'
+        ]
+        color = colors[name.sum % colors.length]
+        
+        content_tag(:div, initials, class: "#{size} rounded-full #{color} text-white flex items-center justify-center font-semibold text-sm select-none")
+      end
     end
   end
 
