a7e43c6303eccb7441f630799fe4fe63d087e5f8
Alicia <aliciapazrojas@gmail.com>
2025-12-15 21:41:00 -0600

    Make migration idempotent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_02_225954) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_15_194445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
