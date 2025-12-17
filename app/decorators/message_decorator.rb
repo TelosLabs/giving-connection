7d43e1e9dd07a61e6828dd0e9704b7c604120fd6
Jorge Adrian de la Garza Flores <jorge@teloslabs.co>
2025-12-17 11:50:30 -0600

    modify instructions and option order

--- app/decorators/message_decorator.rb
+++ app/decorators/message_decorator.rb
@@ -3,9 +3,9 @@ class MessageDecorator < ApplicationDecorator
 
   def real_subject
     case subject
-    when "1" then "I want to add a nonprofit to Giving Connection."
-    when "2" then "I want to claim ownership of a nonprofit profile on Giving Connection."
-    when "3" then "I’m looking for help from a nonprofit organization."
+    when "1" then "I’m looking for help from a nonprofit organization."
+    when "2" then "I want to add a nonprofit to Giving Connection."
+    when "3" then "I want to claim ownership of a nonprofit profile on Giving Connection."
     when "4" then "Other"
     end
   end
