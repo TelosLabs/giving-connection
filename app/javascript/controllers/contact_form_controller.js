f311f3725fe7e56ba83e97f7888fef0b6bfea686
Jorge Adrian de la Garza Flores <jorge@teloslabs.co>
2025-12-17 11:57:17 -0600

    fix default message contact us controller

--- app/javascript/controllers/contact_form_controller.js
+++ app/javascript/controllers/contact_form_controller.js
@@ -6,9 +6,9 @@ export default class extends Controller {
   }
 
   subjectSelected() {
-    if (this.subjectTarget.value === '1') {
+    if (this.subjectTarget.value === '2') {
       this.messageTarget.innerText = 'Hi, I’m interested in publishing a nonprofit on Giving Connection.'
-    } else if (this.subjectTarget.value === '2') {
+    } else if (this.subjectTarget.value === '3') {
       this.messageTarget.innerText = 'Hi, I’m interested in claiming a nonprofit profile already published on Giving Connection.'
     } else {
       return this.messageTarget.innerText = ''
