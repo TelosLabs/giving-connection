b3c925d880e804fd31eee650d3b359495e5bca04
Jorge Adrian de la Garza Flores <jorge@teloslabs.co>
2026-01-13 12:58:25 -0600

    add loose validation for coordinates that may change due to updates

--- spec/services/spreadsheet_import/address_location_parser_spec.rb
+++ spec/services/spreadsheet_import/address_location_parser_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe SpreadsheetImport::AddressLocationParser do
   it "returns the correct coordinates for an example place" do
     result = described_class.new("5800 MARMION WAY, LOS ANGELES, CA, 90042").call
-    expect(result.latitude).to eq(34.1110080)
-    expect(result.longitude).to eq(-118.1923320)
+    expect(result.latitude).to be_within(0.001).of(34.1110080)
+    expect(result.longitude).to be_within(0.001).of(-118.1923320)
   end
 
   it "returns the correct coordinates for an example place" do
     result = described_class.new("2133 N HICKS AVE	LOS ANGELES	CA	90032-3643").call
-    expect(result.latitude).to eq(34.0682933)
-    expect(result.longitude).to eq(-118.1909603)
+    expect(result.latitude).to be_within(0.001).of(34.0682933)
+    expect(result.longitude).to be_within(0.001).of(-118.1909603)
   end
 end
