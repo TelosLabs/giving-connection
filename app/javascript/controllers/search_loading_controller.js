c10ea1f3daf2469346edee1e660be9afee650320
Jorge Adrian de la Garza Flores <jorge@teloslabs.co>
2026-01-21 13:18:32 -0600

    smooth scroll

--- app/javascript/controllers/search_loading_controller.js
+++ app/javascript/controllers/search_loading_controller.js
@@ -4,18 +4,34 @@ export default class extends Controller {
   static targets = ['spinner', 'resultsContainer']
 
   connect () {
-    this.element.addEventListener('turbo:before-fetch-request', () => {
-      this.scrollToTop()
-      this.showSpinner()
-    })
-    
-    this.element.addEventListener('turbo:frame-render', () => {
-      this.hideSpinner()
-    })
-    
-    this.element.addEventListener('turbo:frame-load', () => {
-      this.hideSpinner()
-    })
+    // Store bound functions for proper cleanup
+    this.handleBeforeFetch = this.onBeforeFetch.bind(this)
+    this.handleFrameRender = this.onFrameRender.bind(this)
+    this.handleFrameLoad = this.onFrameLoad.bind(this)
+
+    // Listen for Turbo Frame events
+    this.element.addEventListener('turbo:before-fetch-request', this.handleBeforeFetch)
+    this.element.addEventListener('turbo:frame-render', this.handleFrameRender)
+    this.element.addEventListener('turbo:frame-load', this.handleFrameLoad)
+  }
+
+  disconnect () {
+    this.element.removeEventListener('turbo:before-fetch-request', this.handleBeforeFetch)
+    this.element.removeEventListener('turbo:frame-render', this.handleFrameRender)
+    this.element.removeEventListener('turbo:frame-load', this.handleFrameLoad)
+  }
+
+  onBeforeFetch () {
+    this.scrollToTop()
+    this.showSpinner()
+  }
+
+  onFrameRender () {
+    this.hideSpinner()
+  }
+
+  onFrameLoad () {
+    this.hideSpinner()
   }
 
   showSpinner () {
@@ -32,9 +48,9 @@ export default class extends Controller {
 
   scrollToTop () {
     if (this.hasResultsContainerTarget) {
-      this.resultsContainerTarget.scrollTop = 0
+      this.resultsContainerTarget.scrollTo({ top: 0, behavior: 'smooth' })
     }
     
-    window.scrollTo({ top: 0, behavior: 'instant' })
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 }
