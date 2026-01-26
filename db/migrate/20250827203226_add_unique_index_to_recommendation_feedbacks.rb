91dbcbe458916e966570204658ea641a72f87748
Jorge Adrian de la Garza Flores <jorge@teloslabs.co>
2026-01-26 13:49:09 -0600

    duplicate migrations

--- db/migrate/20250827203226_add_unique_index_to_recommendation_feedbacks.rb
+++
@@ -1,6 +0,0 @@
-class AddUniqueIndexToRecommendationFeedbacks < ActiveRecord::Migration[7.2]
-  def change
-    add_index :recommendation_feedbacks, [:session_id, :nonprofit_id], unique: true
-    add_index :recommendation_feedbacks, :feedback_type
-  end
-end
