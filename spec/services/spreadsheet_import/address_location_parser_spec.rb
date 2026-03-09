e53eed582a53f5fe4d21618f00bd8a23de3eb22f
Alicia Paz <alicia@teloslabs.co>
2026-03-09 14:47:53 -0600

    Stabilize test environment defaults and system specs (#758)
    Co-authored-by: Alicia <alicia@Alicias-MacBook-Pro-2.local>

--- spec/services/spreadsheet_import/address_location_parser_spec.rb
+++ spec/services/spreadsheet_import/address_location_parser_spec.rb
@@ -2,12 +2,20 @@ require "rails_helper"
 
 RSpec.describe SpreadsheetImport::AddressLocationParser do
   it "returns the correct coordinates for an example place" do
+    allow(Geocoder).to receive(:search).with("5800 MARMION WAY, LOS ANGELES, CA, 90042").and_return([
+      Struct.new(:latitude, :longitude).new(34.1110080, -118.1923320)
+    ])
+
     result = described_class.new("5800 MARMION WAY, LOS ANGELES, CA, 90042").call
     expect(result.latitude).to be_within(0.001).of(34.1110080)
     expect(result.longitude).to be_within(0.001).of(-118.1923320)
   end
 
   it "returns the correct coordinates for an example place" do
+    allow(Geocoder).to receive(:search).with("2133 N HICKS AVE	LOS ANGELES	CA	90032-3643").and_return([
+      Struct.new(:latitude, :longitude).new(34.0682933, -118.1909603)
+    ])
+
     result = described_class.new("2133 N HICKS AVE	LOS ANGELES	CA	90032-3643").call
     expect(result.latitude).to be_within(0.001).of(34.0682933)
     expect(result.longitude).to be_within(0.001).of(-118.1909603)
