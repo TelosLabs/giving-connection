eeac2717613882cf6b7c857b9d310253667eab2b
Jorge Adrian de la Garza Flores <jorge@teloslabs.co>
2026-07-22 14:22:04 -0600

    fix trailing schema

--- db/migrate/20260710000000_add_in_kind_donation_link_to_organizations.rb
+++ db/migrate/20260710000000_add_in_kind_donation_link_to_organizations.rb
@@ -1,4 +1,4 @@
-class AddInKindDonationFieldsToOrganizations < ActiveRecord::Migration[7.0]
+class AddInKindDonationLinkToOrganizations < ActiveRecord::Migration[7.2]
   def change
     add_column :organizations, :in_kind_donation_link, :string
   end
